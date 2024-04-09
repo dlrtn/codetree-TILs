6c0452090c8540a46aac25053e25fafe7ea5144c
Icksoo <87226112+dlrtn@users.noreply.github.com>
2024-04-09 15:43:38 +0900

    Title: 행에 따라 다르게 출력, Time: 491ms, Memory: 19MB, Status: Passed - CodeTree

---
+++ 240409/행에 따라 다르게 출력/output-differently-for-row.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i % 2 == 0) {
+                    count++;
+                } else {
+                    count += 2;
+                }
+
+                System.out.printf("%d ", count);
+            }
+            System.out.println();
+        }
+    }
+}
