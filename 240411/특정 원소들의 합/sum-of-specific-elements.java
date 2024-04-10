1b608208fa523255da0203841ca545d4237dce7f
Icksoo <87226112+dlrtn@users.noreply.github.com>
2024-04-11 00:33:05 +0900

    Title: 특정 원소들의 합, Time: 91ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240411/특정 원소들의 합/sum-of-specific-elements.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = null;
+
+        int n = 4;
+        int[][] arr = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < n; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j <= i; j++) {
+                sum += arr[i][j];
+            }
+        }
+
+        System.out.println(sum);
+    }
+}
