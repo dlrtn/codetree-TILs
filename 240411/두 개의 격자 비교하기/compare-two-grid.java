0cc6cab19b1ce3e9da9a60881a5f7ad87bd7290b
Icksoo <87226112+dlrtn@users.noreply.github.com>
2024-04-11 00:36:41 +0900

    Title: 두 개의 격자 비교하기, Time: 107ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240411/두 개의 격자 비교하기/compare-two-grid.java
@@ -0,0 +1,47 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int[][] arr1 = new int[n][m];
+        int[][] arr2 = new int[n][m];
+        int[][] answer = new int[n][m];
+
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < m; j++) {
+                arr1[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < m; j++) {
+                arr2[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (arr1[i][j] != arr2[i][j]) {
+                    answer[i][j] = 1;
+                }
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                System.out.print(answer[i][j] + " ");
+                
+            }
+            System.out.println();
+        }
+    }
+}
