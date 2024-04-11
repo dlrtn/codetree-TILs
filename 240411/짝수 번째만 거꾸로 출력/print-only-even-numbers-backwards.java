66362a0b8ad367e0da558be85c515b4ebb136bd4
Icksoo <87226112+dlrtn@users.noreply.github.com>
2024-04-11 10:23:42 +0900

    Title: 짝수 번째만 거꾸로 출력, Time: 95ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 240411/짝수 번째만 거꾸로 출력/print-only-even-numbers-backwards.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        String str = st.nextToken();
+
+        Stack<String> stack = new Stack<>();
+
+        for (int i = 0; i < str.length(); i++) {
+            if (i % 2 == 1) {
+                stack.push(String.valueOf(str.charAt(i)));
+            }
+        }
+
+        while (!stack.isEmpty()) {
+            System.out.printf("%s", stack.pop());
+        }
+
+    }
+}
