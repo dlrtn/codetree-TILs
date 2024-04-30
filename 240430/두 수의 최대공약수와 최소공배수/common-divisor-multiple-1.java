b805a567306db0e8de8741e030d0583ea0a81af6
Icksoo <87226112+dlrtn@users.noreply.github.com>
2024-04-30 23:21:19 +0900

    Title: 두 수의 최대공약수와 최소공배수, Time: 102ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240430/두 수의 최대공약수와 최소공배수/common-divisor-multiple-1.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int x = Integer.parseInt(st.nextToken());
+        int y = Integer.parseInt(st.nextToken());
+
+        System.out.print(gcd(x, y) + " " + lcm(x, y));
+
+    }
+
+    public static int gcd(int x, int y) {
+        if (y == 0) {
+            return x;
+        } else {
+            return gcd(y, x % y);
+        }
+    }
+
+    public static int lcm(int x, int y) {
+        return x * y / gcd(x, y);
+    }
+}
