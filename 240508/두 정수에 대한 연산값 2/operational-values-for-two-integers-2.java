3081256171e83eb64833d4312436aef92a1866c7
Icksoo <87226112+dlrtn@users.noreply.github.com>
2024-05-08 10:48:14 +0900

    Title: 두 정수에 대한 연산값 2, Time: 92ms, Memory: 8MB, Status: Passed - CodeTree

--- 240508/두 정수에 대한 연산값 2/operational-values-for-two-integers-2.java
+++ 240508/두 정수에 대한 연산값 2/operational-values-for-two-integers-2.java
@@ -8,15 +8,20 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = null;
-
-        st = new StringTokenizer(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
         int a = Integer.parseInt(st.nextToken());
         int b = Integer.parseInt(st.nextToken());
 
-        System.out.println(
-                add10ToSmallNumber(Math.min(a, b)) + " " + multiple2ToBigNumber(Math.max(a, b)));
+        if (a > b) {
+            a = multiple2ToBigNumber(a);
+            b = add10ToSmallNumber(b);
+        } else {
+            a = add10ToSmallNumber(a);
+            b = multiple2ToBigNumber(b);
+        }
+
+        System.out.println(a + " " + b);
     }
 
     public static int add10ToSmallNumber(int smallNumber) {
