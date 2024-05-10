5a53703080a051dd5899b5e1a846c07fa2c03d52
Icksoo <87226112+dlrtn@users.noreply.github.com>
2024-05-10 14:59:01 +0900

    Title: 함수를 이용한 연속부분수열 여부 판단하기, Time: 82ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240510/함수를 이용한 연속부분수열 여부 판단하기/to-determine-whether-a-continuous-subsequence-is-made-using-a-function.java
@@ -0,0 +1,47 @@
+import static java.lang.System.exit;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n1 = Integer.parseInt(st.nextToken());
+        int n2 = Integer.parseInt(st.nextToken());
+
+        int[] arr1 = new int[n1];
+        int[] arr2 = new int[n2];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n1; i++) {
+            arr1[i] = Integer.parseInt(st.nextToken());
+        }
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n2; i++) {
+            arr2[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for (int i = 0; i < n1 - n2; i++) {
+            int count = 0;
+            for (int j = 0; j < n2; j++) {
+                if (arr1[i + j] == arr2[j]) {
+                    count++;
+                }
+            }
+            if (count == n2) {
+                System.out.println("Yes");
+                exit(0);
+            }
+
+        }
+        System.out.println("No");
+    }
+
+}
