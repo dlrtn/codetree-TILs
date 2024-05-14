926d18ba1cd88c99108cd31eac65cc2cf07492a2
Icksoo <87226112+dlrtn@users.noreply.github.com>
2024-05-14 14:10:04 +0900

    Title: 100으로 나눈 나머지의 수열, Time: 156ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240514/100으로 나눈 나머지의 수열/sequence-of-remainder-divided-by-100.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static int[] arr;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+
+        System.out.println(recursive(n));
+    }
+
+    private static int recursive(int n) {
+        if (n == 1) {
+            return 2;
+        }
+
+        if (n == 2) {
+            return 4;
+        }
+
+        return (recursive(n - 1) * recursive(n - 2)) % 100;
+     }
+}
