942202366c5ef75492716bd5fafba39cb1f77d61
Icksoo <87226112+dlrtn@users.noreply.github.com>
2024-05-14 13:50:45 +0900

    Title: 홀수 짝수에 따른 출력값, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240514/홀수 짝수에 따른 출력값/output-value-based-on-odd-even-numbers.java
@@ -0,0 +1,43 @@
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+    }
+}import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        if (n % 2 ==0) {
+            System.out.println(recursiveEven(n));
+        } else {
+            System.out.println(recursiveOdd(n));
+        
+        }
+
+    }
+
+    private static int recursiveEven(int n) {
+        if (n == 2) {
+            return 2;
+        }
+
+        return recursiveEven(n - 2) + n;
+    }
+
+    public static int recursiveOdd(int n) {
+        if (n == 1) {
+            return 1;
+        }
+
+        return recursiveOdd(n - 2) + n;
+    }
+
+}
