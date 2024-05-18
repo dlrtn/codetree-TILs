7d2c7236d1d58bc6f802f67c0d6966f9e144fe62
Icksoo <87226112+dlrtn@users.noreply.github.com>
2024-05-18 22:14:52 +0900

    Title: Date to Date, Time: 94ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240518/Date to Date/date-to-date.java
@@ -0,0 +1,61 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+        int d = Integer.parseInt(st.nextToken());
+
+        int count = d - b + 1;
+
+        for (int i = a; i < c; i++) {
+            count += month.values()[i - 1].getDay();
+        }
+
+        System.out.println(count);
+
+
+
+    }
+
+}
+
+enum month {
+JANUARY(1, 31),
+    FEBRUARY(2, 28),
+    MARCH(3, 31),
+    APRIL(4, 30),
+    MAY(5, 31),
+    JUNE(6, 30),
+    JULY(7, 31),
+    AUGUST(8, 31),
+    SEPTEMBER(9, 30),
+    OCTOBER(10, 31),
+    NOVEMBER(11, 30),
+    DECEMBER(12, 31);
+
+    private final int month;
+    private final int day;
+
+    month(int month, int day) {
+        this.month = month;
+        this.day = day;
+    }
+
+    public int getMonth() {
+        return month;
+    }
+
+    public int getDay() {
+        return day;
+    }
+}
