63931300c1eeeac7aa09a22ba97a1cdf660b963d
Icksoo <87226112+dlrtn@users.noreply.github.com>
2024-05-20 10:34:36 +0900

    Title: 왔다 갔던 구역 2, Time: 265ms, Memory: 14MB, Status: Wrong Answer - CodeTree

--- 240520/왔다 갔던 구역 2/area-been-to-and-from2.java
+++ 240520/왔다 갔던 구역 2/area-been-to-and-from2.java
@@ -24,18 +24,32 @@ public class Main {
             String D = st.nextToken();
 
             if (D.equals("R")) {
-                for (int j = 0; j < x; j++) {
+                for (int j = 0; j <= x; j++) {
                     arr[start + j]++;
                 }
                 start += x;
             } else {
-                for (int j = 0; j < x; j++) {
+                for (int j = 0; j <= x; j++) {
                     arr[start - j]++;
                 }
                 start -= x;
             }
+            System.out.println(start);
         }
 
-        System.out.println(Arrays.stream(arr).filter(i -> i >= 2).count());
+        int count = 0;
+        boolean flag = false;
+        for (int i = 0; i < 2001; i++) {
+            if (arr[i] == 0) {
+            } else if (arr[i] >= 2 && !flag) {
+                // 구간 시작
+                flag = true;
+                count++;
+            } else if (flag && arr[i] < 2) {
+                flag = false;
+            }
+        }
+
+        System.out.println(Arrays.stream(arr).filter(i -> i >= 2).count() - count);
     }
 }
