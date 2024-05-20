c5fb40c7adcffc10d14899caedc84654c10167fb
Icksoo <87226112+dlrtn@users.noreply.github.com>
2024-05-20 11:43:36 +0900

    Title: 잔해물을 덮기 위한 사각형의 최소 넓이, Time: 230ms, Memory: 26MB, Status: Passed - CodeTree

--- 240520/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.java
+++ 240520/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.java
@@ -1,59 +1,64 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.Scanner;
 
 public class Main {
+    public static final int OFFSET = 1000;
+    public static final int MAX_R = 2000;
+    public static final int N = 2;
+    
+    public static int[] x1 = new int[N];
+    public static int[] y1 = new int[N];
+    public static int[] x2 = new int[N];
+    public static int[] y2 = new int[N];
+    
+    public static int[][] checked = new int[MAX_R + 1][MAX_R + 1];
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = null;
-
-        int[][] arr = new int[2001][2001];
-
-        for (int i = 0; i < 2; i++) {
-            st = new StringTokenizer(br.readLine());
-            int x1 = Integer.parseInt(st.nextToken()) + 1000;
-            int y1 = Integer.parseInt(st.nextToken()) + 1000;
-            int x2 = Integer.parseInt(st.nextToken()) + 1000;
-            int y2 = Integer.parseInt(st.nextToken()) + 1000;
-
-            if (i == 0) {
-                for (int x = x1; x <= x2; x++) {
-                    for (int y = y1; y <= y2; y++) {
-                        arr[x][y] = 1;
-                    }
-                }
-            }
-            if (i == 1) {
-                for (int x = x1; x < x2; x++) {
-                    for (int y = y1; y < y2; y++) {
-                        arr[x][y] = 0;
-                    }
-                }
-            }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // 입력
+        for(int i = 0; i < N; i++) {
+            x1[i] = sc.nextInt();
+            y1[i] = sc.nextInt();
+            x2[i] = sc.nextInt();
+            y2[i] = sc.nextInt();
+            
+            // OFFSET을 더해줍니다.
+            x1[i] += OFFSET;
+            y1[i] += OFFSET;
+            x2[i] += OFFSET;
+            y2[i] += OFFSET;
         }
-
-        int minX = 2001;
-        int minY = 2001;
-        int maxX = 0;
-        int maxY = 0;
-
-        for (int i = 0; i < 2001; i++) {
-            for (int j = 0; j < 2001; j++) {
-                if (arr[i][j] == 1) {
-                    minX = Math.min(minX, i);
-                    minY = Math.min(minY, j);
-                    maxX = Math.max(maxX, i);
-                    maxY = Math.max(maxY, j);
+        
+        // 직사각형에 주어진 순으로 1, 2 번호를 붙여줍니다.
+        // 격자 단위로 진행하는 문제이므로
+        // x2, y2에 등호가 들어가지 않음에 유의합니다.
+        for(int i = 0; i < N; i++)
+            for(int x = x1[i]; x < x2[i]; x++)
+                for(int y = y1[i]; y < y2[i]; y++)
+                    checked[x][y] = i + 1;
+        
+        // 1, 2 순으로 붙였는데도
+        // 아직 숫자 1로 남아있는 곳들 중 최대 최소 x, y 값을 전부 계산합니다.
+        int minX = MAX_R, maxX = 0, minY = MAX_R, maxY = 0;
+        boolean firstRectExist = false;
+        for(int x = 0; x <= MAX_R; x++)
+            for(int y = 0; y <= MAX_R; y++)
+                if(checked[x][y] == 1) {
+                    firstRectExist = true;
+                    minX = Math.min(minX, x);
+                    maxX = Math.max(maxX, x);
+                    minY = Math.min(minY, y);
+                    maxY = Math.max(maxY, y);
                 }
-            }
-        }
-        if (minX == 2001 || minY == 2001 || maxX == 0 || maxY == 0) {
-            System.out.println(0);
-            return;
-        }
+        
+        // 넓이를 계산합니다.
+        int area;
+        // Case 1. 첫 번째 직사각형이 전혀 남아있지 않다면 넓이는 0입니다.
+        if(!firstRectExist)
+            area = 0;
+        // Case 2. 첫 번째 직사각형이 남아있다면, 넓이를 계산합니다.
+        else
+            area = (maxX - minX + 1) * (maxY - minY + 1);
 
-        System.out.println((maxX - minX) * (maxY - minY));
+        System.out.print(area);
     }
 }
