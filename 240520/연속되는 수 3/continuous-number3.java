7dd7aad506756bc36da98c21ef11a99230d38dee
Icksoo <87226112+dlrtn@users.noreply.github.com>
2024-05-20 11:48:46 +0900

    Title: 연속되는 수 3, Time: 183ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 240520/연속되는 수 3/continuous-number3.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int temp = 0;
+        int count = 1;
+        int answer = 0;
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+
+            if ((temp < 0 && a < 0) || (temp > 0 && a > 0)) {
+                count++;
+                answer = Math.max(answer, count);
+            } else {
+                answer = Math.max(answer, count);
+                count = 1;
+            }
+            temp = a;
+
+        }
+
+        System.out.println(answer);
+    }
+}
