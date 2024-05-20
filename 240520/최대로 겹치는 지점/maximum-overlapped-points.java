d33a59a995b2eee71aafce1cf0df427a775a2830
Icksoo <87226112+dlrtn@users.noreply.github.com>
2024-05-20 10:12:04 +0900

    Title: 최대로 겹치는 지점, Time: 261ms, Memory: 14MB, Status: Passed - CodeTree

---
+++ 240520/최대로 겹치는 지점/maximum-overlapped-points.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static int[] arr;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[201];
+
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            for (int j = a; j <= b; j++) {
+                arr[101 + j - 1] += 1;
+            }
+        }
+
+        System.out.println(Arrays.stream(arr).max().getAsInt());
+    }
+}
