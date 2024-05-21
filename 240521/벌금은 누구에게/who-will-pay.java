f5360f168e90cf143061c7f9f51071d892ba1c98
Icksoo <87226112+dlrtn@users.noreply.github.com>
2024-05-21 13:49:12 +0900

    Title: 벌금은 누구에게, Time: 123ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240521/벌금은 누구에게/who-will-pay.java
@@ -0,0 +1,36 @@
+import static java.lang.System.exit;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[n];
+
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            int a = Integer.parseInt(st.nextToken());
+            arr[a-1]++;
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (arr[i] >= k) {
+                System.out.println(i+1);
+                exit(0);
+            }
+        }
+        System.out.println(-1);
+    }
+
+}
