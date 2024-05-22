b63aaee5bf7996788dda1411b9e613b7d20204c0
Icksoo <87226112+dlrtn@users.noreply.github.com>
2024-05-22 13:56:54 +0900

    Title: 격자 위의 편안한 상태, Time: 172ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240522/격자 위의 편안한 상태/comfortable-state-on-the-grid.java
@@ -0,0 +1,51 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        int[][] arr = new int[n][n];
+
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            int r = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+
+            r -= 1;
+            c -= 1;
+
+            arr[r][c] = 1;
+
+            int count = 0;
+            if (r - 1 >= 0 && arr[r - 1][c] == 1) {
+                count++;
+            }
+            if (r + 1 < n && arr[r + 1][c] == 1) {
+                count++;
+            }
+            if (c - 1 >= 0 && arr[r][c - 1] == 1) {
+                count++;
+            }
+            if (c + 1 < n && arr[r][c + 1] == 1) {
+                count++;
+            }
+
+            if (count >= 3) {
+                System.out.println(1);
+            } else {
+                System.out.println(0);
+            }
+
+        }
+    }
+
+}
