804f085247172feb1aaec924d344674108920b77
Icksoo <87226112+dlrtn@users.noreply.github.com>
2024-05-23 16:10:45 +0900

    Title: 특정 수와 근접한 합, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240523/특정 수와 근접한 합/sum-close-to-particular-number.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int s = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[n];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int min = Integer.MAX_VALUE;
+        int totalSum = Arrays.stream(arr).sum();
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (Math.abs(totalSum - arr[i] - arr[j] - s) < min) {
+                    min = Math.abs(totalSum - arr[i] - arr[j] - s);
+                }
+            }
+        }
+
+        System.out.println(min)
+
+    }
+}
