cbad6a389bf6c098b1c9a6354ae9c6f6d1985a5e
Icksoo <87226112+dlrtn@users.noreply.github.com>
2024-05-23 13:56:07 +0900

    Title: 체크판위에서 2, Time: 98ms, Memory: 8MB, Status: Passed - CodeTree

--- 240523/체크판위에서 2/on-the-checkboard-2.java
+++ 240523/체크판위에서 2/on-the-checkboard-2.java
@@ -1,6 +1,8 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -20,15 +22,55 @@ public class Main {
             }
         }
 
-        int x = 0;
-        int y = 0;
+        Node startNode = new Node(0, 0, map[0][0]);
+        Node endNode = new Node(r - 1, c - 1, map[r - 1][c - 1]);
 
-        if (map[0][0].equals(map[r-1][c-1])) {
-            System.out.println(0);
-        } else {
-            
+        int count = 0;
+
+        List<Node> nodes = new ArrayList<>();
+
+        // 첫 번째 점프 가능성 확인
+        for (int i = 1; i < r - 2; i++) {
+            for (int j = 1; j < c - 2; j++) {
+                if (!map[i][j].equals(startNode.value)) {
+                    nodes.add(new Node(i, j, map[i][j]));
+                }
+            }
         }
 
+        List<Node> nodes2 = new ArrayList<>();
+        // 두 번째 점프 가능성 확인
+        for (Node node : nodes) {
+            for (int i = node.x + 1; i < r - 1; i++) {
+                for (int j = node.y + 1; j < c - 1; j++) {
+                    if (!map[i][j].equals(node.value)) {
+                        nodes2.add(new Node(i, j, map[i][j]));
+                    }
+                }
+            }
+        }
+
+        // 세 번째 점프 가능성 확인
+        for (Node node : nodes2) {
+            if (!endNode.value.equals(node.value)) {
+                count++;
+            }
+        }
+
+        System.out.println(count);
+    }
+
+    public static class Node {
+
+        int x;
+        int y;
+        String value;
+
+        public Node(int x, int y, String value) {
+            this.x = x;
+            this.y = y;
+            this.value = value;
+        }
     }
 
 
