710e3e913b4eff205871511abef6b646364faeee
Icksoo <87226112+dlrtn@users.noreply.github.com>
2024-05-23 14:48:14 +0900

    Title: Carry 피하기 2, Time: 91ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240523/Carry 피하기 2/escaping-carry-2.java
@@ -0,0 +1,49 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        int count = 0;
+        for (int i = 0; i < n - 2; i++) {
+            for (int j = i + 1; j < n - 1; j++) {
+                for (int k = j + 1; k < n; k++) {
+                    int a = arr[i];
+                    int b = arr[j];
+                    int c = arr[k];
+                    boolean isCarry = false;
+                    for (int l = 0; l < 5; l++) {
+                        if (isCarry(a % 10, b % 10, c % 10)) {
+                            isCarry = true;
+                            break;
+                        }
+                        a /= 10;
+                        b /= 10;
+                        c /= 10;
+                    }
+                    if (!isCarry) {
+                        count = Math.max(count, arr[i] + arr[j] + arr[k]);
+                    }
+                }
+            }
+        }
+
+        System.out.println(count);
+    }
+
+    public static boolean isCarry(int a, int b, int c) {
+        return a + b + c >= 10;
+    }
+}
