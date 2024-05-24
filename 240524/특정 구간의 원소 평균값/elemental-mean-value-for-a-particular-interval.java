f6fc5c886e0ce2a1f9d62446077da0c3b7134d42
Icksoo <87226112+dlrtn@users.noreply.github.com>
2024-05-24 15:25:52 +0900

    Title: 특정 구간의 원소 평균값, Time: 158ms, Memory: 10MB, Status: Passed - CodeTree

--- 240524/특정 구간의 원소 평균값/elemental-mean-value-for-a-particular-interval.java
+++ 240524/특정 구간의 원소 평균값/elemental-mean-value-for-a-particular-interval.java
@@ -1,41 +1,41 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.Scanner;
 
 public class Main {
+    public static final int MAX_N = 100;
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+    public static int n;
+    public static int[] arr = new int[MAX_N];
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-        int n = Integer.parseInt(st.nextToken());
-
-        int[] arr = new int[n];
-        st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < n; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
-        }
-
-        int count = n;
-        for (int i = 1; i < n; i++) {
-            for (int j = 0; j < n - i; j++) {
+        // 입력
+        n = sc.nextInt();
+        for(int i = 0; i < n; i++)
+            arr[i] = sc.nextInt();
+        
+        // 모든 구간을 잡아봅니다.
+        int cnt = 0;
+        for(int i = 0; i < n; i++) // 구간의 시작점을 잡아봅니다.
+            for(int j = i; j < n; j++) { // 구간의 끝점을 잡아봅니다.
+                // 구간 [i, j] 내 원소의 합을 구합니다.
                 int sum = 0;
-                for (int k = 0; k <= i; k++) {
-                    sum += arr[j + k];
-                }
-                double avg = (double) sum / i;
-
-                for (int k = 0; k <= i; k++) {
-                    if (arr[j + k] == avg) {
-                        count++;
-                        break;
-                    }
-                }
+                for(int k = i; k <= j; k++)
+                    sum += arr[k];
+                
+                // 평균을 구합니다.
+                double avg = (double)sum / (j - i + 1);
+                
+                // 구간 내에 원소 평균값과 동일한 원소가 있으면 개수를 세줍니다.
+                boolean exists = false;
+                for(int k = i; k <= j; k++)
+                    if(arr[k] == avg)
+                        exists = true;
+                
+                if(exists)
+                    cnt++;
             }
-        }
-
-        System.out.println(count);
+        
+        System.out.println(cnt);
     }
-
 }
