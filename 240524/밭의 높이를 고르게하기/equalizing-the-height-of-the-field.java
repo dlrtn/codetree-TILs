d0a96325e38ebf782be841e1d01d65d065961532
Icksoo <87226112+dlrtn@users.noreply.github.com>
2024-05-24 16:13:57 +0900

    Title: 밭의 높이를 고르게하기, Time: 91ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240524/밭의 높이를 고르게하기/equalizing-the-height-of-the-field.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int h = Integer.parseInt(st.nextToken());
+        int t = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int min = Integer.MAX_VALUE;
+        for (int i = 0; i < n - t; i++) {
+            int sum = 0;
+            for (int j = i; j < i + t; j++) {
+                sum += Math.abs(arr[j] - h);
+            }
+            min = Math.min(min, sum);
+        }
+
+        System.out.println(min);
+    }
+
+
+}
