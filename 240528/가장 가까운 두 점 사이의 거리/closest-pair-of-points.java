69e8380d264061cfea8df155ee12a6992eecff69
Icksoo <87226112+dlrtn@users.noreply.github.com>
2024-05-28 13:53:47 +0900

    Title: 가장 가까운 두 점 사이의 거리, Time: 110ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240528/가장 가까운 두 점 사이의 거리/closest-pair-of-points.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+
+        int[] x = new int[n];
+        int[] y = new int[n];
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            x[i] = Integer.parseInt(st.nextToken());
+            y[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int minDistance = Integer.MAX_VALUE;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == j) {
+                    continue;
+                }
+
+                int distance = (x[i] - x[j]) * (x[i] - x[j]) + (y[i] - y[j]) * (y[i] - y[j]);
+
+                minDistance = Math.min(minDistance, distance);
+            }
+        }
+
+        System.out.println(minDistance);
+    }
+}
