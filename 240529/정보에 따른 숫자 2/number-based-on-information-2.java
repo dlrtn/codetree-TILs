60631056e0efbe46da07051ff40ea5263a57dbb0
Icksoo <87226112+dlrtn@users.noreply.github.com>
2024-05-29 14:40:43 +0900

    Title: 정보에 따른 숫자 2, Time: 86ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240529/정보에 따른 숫자 2/number-based-on-information-2.java
@@ -0,0 +1,53 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int t = Integer.parseInt(st.nextToken());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+
+        List<Integer> sArr = new ArrayList<>();
+        List<Integer> nArr = new ArrayList<>();
+        for (int i = 0; i < t; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            String s = st.nextToken();
+            int n = Integer.parseInt(st.nextToken());
+
+            if (s.equals("S")) {
+                sArr.add(n);
+            } else {
+                nArr.add(n);
+            }
+        }
+
+        int answer = 0;
+        for (int i = a; i <= b; i++) {
+            int minD1 = Integer.MAX_VALUE;
+            for (Integer s : sArr) {
+                minD1 = Math.min(minD1, Math.abs(a + i - s));
+            }
+
+            int minD2 = Integer.MAX_VALUE;
+            for (Integer n : nArr) {
+                minD2 = Math.min(minD2, Math.abs(a + i - n));
+            }
+
+            if (minD1 <= minD2) {
+                answer++;
+            }
+
+        }
+
+        System.out.println(answer);
+    }
+}
