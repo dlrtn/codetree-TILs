880a4abfcdbd24fb7b20a68d102e59114ad95994
Icksoo <87226112+dlrtn@users.noreply.github.com>
2024-05-29 13:56:40 +0900

    Title: 숫자들의 합 중 최대, Time: 82ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240529/숫자들의 합 중 최대/maximum-of-sum-of-numbers.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int x = Integer.parseInt(st.nextToken());
+        int y = Integer.parseInt(st.nextToken());
+
+        int max = 0;
+
+        for (int i = x; i <= y; i++) {
+            int sum = get(i);
+
+            max = Math.max(max, sum);
+        }
+        System.out.println(max);
+    }
+
+    public static int get(int n) {
+        int sum = 0;
+        while (n > 0) {
+            sum += n % 10;
+            n /= 10;
+        }
+        return sum;
+    }
+}
