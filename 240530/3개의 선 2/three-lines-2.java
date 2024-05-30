654a619926ceb6f16ba5eedd0cbd9a85e327dfec
Icksoo <87226112+dlrtn@users.noreply.github.com>
2024-05-30 14:48:19 +0900

    Title: 3개의 선 2, Time: 141ms, Memory: 10MB, Status: Passed - CodeTree

--- 240530/3개의 선 2/three-lines-2.java
+++ 240530/3개의 선 2/three-lines-2.java
@@ -6,6 +6,7 @@ import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -22,19 +23,110 @@ public class Main {
         }
 
         int answer = 0;
+        // xxx
         for (int j = 0; j <= 10; j++) {
             for (int k = 0; k <= 10; k++) {
                 for (int i = 0; i <= 10; i++) {
-                    int count = 0;
+                    boolean[] visited = new boolean[n];
                     for (Point point : points) {
                         int x = point.x;
                         int y = point.y;
 
-                        if (x == i || x == k || x == j || y == i || y == k || y == j) {
+                        if (x == i || x == j || x == k) {
+                            visited[points.indexOf(point)] = true;
+                        }
+                    }
+
+                    int count = 0;
+                    for (int l = 0; l < visited.length; l++) {
+                        if (visited[l]) {
                             count++;
                         }
                     }
-                    if (count >= n) {
+
+                    if (count == n) {
+                        answer = 1;
+                    }
+                }
+            }
+        }
+        // xxy
+        for (int j = 0; j <= 10; j++) {
+            for (int k = 0; k <= 10; k++) {
+                for (int i = 0; i <= 10; i++) {
+                    boolean[] visited = new boolean[n];
+                    for (Point point : points) {
+                        int x = point.x;
+                        int y = point.y;
+
+                        if (x == i || x == j || y == k) {
+                            visited[points.indexOf(point)] = true;
+                        }
+                    }
+
+                    int count = 0;
+                    for (int l = 0; l < visited.length; l++) {
+                        if (visited[l]) {
+                            count++;
+                        }
+                    }
+
+                    if (count == n) {
+                        answer = 1;
+                    }
+                }
+            }
+        }
+
+        // xyy
+        for (int j = 0; j <= 10; j++) {
+            for (int k = 0; k <= 10; k++) {
+                for (int i = 0; i <= 10; i++) {
+                    boolean[] visited = new boolean[n];
+                    for (Point point : points) {
+                        int x = point.x;
+                        int y = point.y;
+
+                        if (x == i || y == j || y == k) {
+                            visited[points.indexOf(point)] = true;
+                        }
+                    }
+
+                    int count = 0;
+                    for (int l = 0; l < visited.length; l++) {
+                        if (visited[l]) {
+                            count++;
+                        }
+                    }
+
+                    if (count == n) {
+                        answer = 1;
+                    }
+                }
+            }
+        }
+        // yyy
+        for (int j = 0; j <= 10; j++) {
+            for (int k = 0; k <= 10; k++) {
+                for (int i = 0; i <= 10; i++) {
+                    boolean[] visited = new boolean[n];
+                    for (Point point : points) {
+                        int x = point.x;
+                        int y = point.y;
+
+                        if (y == i || y == j || y == k) {
+                            visited[points.indexOf(point)] = true;
+                        }
+                    }
+
+                    int count = 0;
+                    for (int l = 0; l < visited.length; l++) {
+                        if (visited[l]) {
+                            count++;
+                        }
+                    }
+
+                    if (count == n) {
                         answer = 1;
                     }
                 }
