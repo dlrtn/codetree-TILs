5b9b0f5d035fc51c3c44ea287f3b92a03dcf74ac
Icksoo <87226112+dlrtn@users.noreply.github.com>
2024-05-30 15:29:07 +0900

    Title: 팀으로 하는 틱택토 2, Time: 95ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240530/팀으로 하는 틱택토 2/tic-tac-to-as-a-team-2.java
@@ -0,0 +1,78 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = null;
+
+        int[][] arr = new int[3][3];
+
+        for (int i = 0; i < 3; i++) {
+            st = new StringTokenizer(br.readLine());
+            String[] temp = st.nextToken().split("");
+            for (int j = 0; j < 3; j++) {
+                arr[i][j] = temp[j].charAt(0) - '0';
+            }
+        }
+
+        int count = 0;
+        for (int i = 1; i < 10; i++) {
+            for (int j = i + 1; j < 10; j++) {
+                // 1 2 3
+                if ((arr[0][0] == i || arr[0][0] == j) && ((arr[0][1] == i) || (arr[0][1] == j)) && ((arr[0][2] == i) || (arr[0][2] == j))
+                        && !(arr[0][0] == i && arr[0][1] == i && arr[0][2] == i) && !(arr[0][0] == j && arr[0][1] == j && arr[0][2] == j)) {
+                    count++;
+                }
+
+                // 4 5 6
+                if ((arr[1][0] == i || arr[1][0] == j) && ((arr[1][1] == i) || (arr[1][1] == j)) && ((arr[1][2] == i) || (arr[1][2] == j))
+                        && !(arr[1][0] == i && arr[1][1] == i && arr[1][2] == i) && !(arr[1][0] == j && arr[1][1] == j && arr[1][2] == j)) {
+                    count++;
+                }
+
+                // 7 8 9
+                if ((arr[2][0] == i || arr[2][0] == j) && ((arr[2][1] == i) || (arr[2][1] == j)) && ((arr[2][2] == i) || (arr[2][2] == j))
+                        && !(arr[2][0] == i && arr[2][1] == i && arr[2][2] == i) && !(arr[2][0] == j && arr[2][1] == j && arr[2][2] == j)) {
+                    count++;
+                }
+
+                // 1 4 7
+                if ((arr[0][0] == i || arr[0][0] == j) && ((arr[1][0] == i) || (arr[1][0] == j)) && ((arr[2][0] == i) || (arr[2][0] == j))
+                        && !(arr[0][0] == i && arr[1][0] == i && arr[2][0] == i) && !(arr[0][0] == j && arr[1][0] == j && arr[2][0] == j)) {
+                    count++;
+                }
+
+                // 2 5 8
+                if ((arr[0][1] == i || arr[0][1] == j) && ((arr[1][1] == i) || (arr[1][1] == j)) && ((arr[2][1] == i) || (arr[2][1] == j))
+                        && !(arr[0][1] == i && arr[1][1] == i && arr[2][1] == i) && !(arr[0][1] == j && arr[1][1] == j && arr[2][1] == j)) {
+                    count++;
+                }
+
+                // 3 6 9
+                if ((arr[0][2] == i || arr[0][2] == j) && ((arr[1][2] == i) || (arr[1][2] == j)) && ((arr[2][2] == i) || (arr[2][2] == j))
+                        && !(arr[0][2] == i && arr[1][2] == i && arr[2][2] == i) && !(arr[0][2] == j && arr[1][2] == j && arr[2][2] == j)) {
+                    count++;
+                }
+
+                // 1 5 9
+                if ((arr[0][0] == i || arr[0][0] == j) && ((arr[1][1] == i) || (arr[1][1] == j)) && ((arr[2][2] == i) || (arr[2][2] == j))
+                        && !(arr[0][0] == i && arr[1][1] == i && arr[2][2] == i) && !(arr[0][0] == j && arr[1][1] == j && arr[2][2] == j)) {
+                    count++;
+                }
+
+                // 3 5 7
+                if ((arr[0][2] == i || arr[0][2] == j) && ((arr[1][1] == i) || (arr[1][1] == j)) && ((arr[2][0] == i) || (arr[2][0] == j))
+                        && !(arr[0][2] == i && arr[1][1] == i && arr[2][0] == i) && !(arr[0][2] == j && arr[1][1] == j && arr[2][0] == j)) {
+                    count++;
+                }
+            }
+
+        }
+
+        System.out.println(count);
+    }
+}
