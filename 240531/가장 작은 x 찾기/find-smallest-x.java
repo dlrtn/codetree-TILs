b8354b9bbedf4cfacdf434683ea788de0a239898
Icksoo <87226112+dlrtn@users.noreply.github.com>
2024-05-31 14:05:49 +0900

    Title: 가장 작은 x 찾기, Time: 89ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240531/가장 작은 x 찾기/find-smallest-x.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+
+        int[] a = new int[n];
+        int[] b = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            a[i] = Integer.parseInt(st.nextToken());
+            b[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for (int i = a[0]; i <= b[0]; i++) {
+            int value = i * 2;
+            int k = 0;
+
+            while (value >= a[k] && value <= b[k]) {
+                k++;
+                value *= 2;
+
+                if (k == n) {
+                    System.out.println(i);
+                    return;
+                }
+            }
+        }
+    }
+}
