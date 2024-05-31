5f832551f5b3fe88ff93a31f660968f99ffefc7e
Icksoo <87226112+dlrtn@users.noreply.github.com>
2024-05-31 14:08:05 +0900

    Title: 가장 작은 x 찾기, Time: 103ms, Memory: 8MB, Status: Passed - CodeTree

--- 240531/가장 작은 x 찾기/find-smallest-x.java
+++ 240531/가장 작은 x 찾기/find-smallest-x.java
@@ -21,7 +21,7 @@ public class Main {
             b[i] = Integer.parseInt(st.nextToken());
         }
 
-        for (int i = a[0]; i <= b[0]; i++) {
+        for (int i = 0; i <= b[0] / 2; i++) {
             int value = i * 2;
             int k = 0;
 
