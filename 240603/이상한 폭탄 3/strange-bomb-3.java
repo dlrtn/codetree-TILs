350614539275c90f62b5155165a3a9e7e61f58a7
Icksoo <87226112+dlrtn@users.noreply.github.com>
2024-06-03 13:53:52 +0900

    Title: 이상한 폭탄 3, Time: 89ms, Memory: 8MB, Status: Wrong Answer - CodeTree

--- 240603/이상한 폭탄 3/strange-bomb-3.java
+++ 240603/이상한 폭탄 3/strange-bomb-3.java
@@ -1,9 +1,13 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.StringTokenizer;
 
 public class Main {
+
+    public static Map<Integer, Integer> map = new HashMap<>();
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -17,12 +21,22 @@ public class Main {
             arr[i] = Integer.parseInt(br.readLine());
         }
 
-        int answer = 0;
         for (int i = 0; i < n; i++) {
             for (int j = i + 1; j < n; j++) {
-                if (arr[i] == arr[j] && j - i < k) {
-                    answer = Math.max(answer, arr[i]);
+                if (arr[i] == arr[j] && j - i <= k) {
+                    if (!map.containsKey(arr[i])) {
+                        map.put(arr[i], 1);
+                    } else {
+                        map.put(arr[i], map.get(arr[i]) + 1);
+                    }
                 }
+            }
+        }
+
+        int answer = 0;
+        for (int i : map.keySet()) {
+            if (answer <= map.get(i)) {
+                answer = Math.max(answer, i);
             }
         }
 
