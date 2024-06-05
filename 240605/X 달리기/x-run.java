6d0ec31958ec4c857ac4a38d67585074b9475a13
Icksoo <87226112+dlrtn@users.noreply.github.com>
2024-06-05 14:45:03 +0900

    Title: X 달리기, Time: 93ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240605/X 달리기/x-run.java
@@ -0,0 +1,49 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int x = Integer.parseInt(st.nextToken());
+
+        List<Distance> distances = new ArrayList<>();
+        for (int i = 1; i <= 100; i++) {
+            // 소요 시간 2n - 1
+            // i 제곱 값이 해당 거리
+
+            distances.add(new Distance(i * i, 2 * i - 1));
+        }
+
+        int count = 0;
+        while (x != 0) {
+            for (int i = 100; i > 0; i--) {
+                if (x >= distances.get(i - 1).distance) {
+                    x -= distances.get(i - 1).distance;
+                    count += distances.get(i - 1).time;
+                    break;
+                }
+            }
+        }
+
+        System.out.println(count);
+    }
+
+    static class Distance {
+        int distance;
+        int time;
+
+        public Distance(int distance, int time) {
+            this.distance = distance;
+            this.time = time;
+        }
+    }
+}
