ea3eb02d3a92ed8ba0373a58903f9d88d702c26d
Icksoo <87226112+dlrtn@users.noreply.github.com>
2024-06-07 15:05:21 +0900

    Title: 독서실의 거리두기 2, Time: 91ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240607/독서실의 거리두기 2/study-cafe-keeping-distance-2.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine());
+        String str = st.nextToken();
+        boolean[] humans = new boolean[n];
+
+        for (int i = 0; i < n; i++) {
+            if (str.charAt(i) == '1') {
+                humans[i] = true;
+            }
+        }
+
+        int maxDistance = 0;
+        int minDistance = Integer.MAX_VALUE;
+        for (int i = 0; i < humans.length; i++) {
+            if (humans[i]) {
+                for (int j = i + 1; j < humans.length; j++) {
+                    if (humans[j]) {
+                        maxDistance = Math.max(maxDistance, j - i);
+                        minDistance = Math.min(minDistance, j - i);
+                        break;
+                    }
+                }
+            }
+        }
+
+        System.out.println(Math.min(maxDistance / 2, minDistance));
+    }
+}
