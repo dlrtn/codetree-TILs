5a6ec8c17b557891da79d68143c004c7cdf109ca
Icksoo <87226112+dlrtn@users.noreply.github.com>
2024-06-07 15:09:43 +0900

    Title: ABC 줄 세우기, Time: 93ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240607/ABC 줄 세우기/abc-line-up.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine());
+        char[] arr = new char[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = st.nextToken().charAt(0);
+        }
+
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+            if (arr[i] != 'A' + i) {
+                count++;
+            }
+        }
+
+        System.out.println(count - 1);
+    }
+}
