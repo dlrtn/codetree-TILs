a48c559e6602f317404be117c72129f4efb06e4c
Icksoo <87226112+dlrtn@users.noreply.github.com>
2024-06-07 14:37:18 +0900

    Title: 독서실의 거리두기 3, Time: 91ms, Memory: 8MB, Status: Wrong Answer - CodeTree

--- 240607/독서실의 거리두기 3/study-cafe-keeping-distance-3.java
+++ 240607/독서실의 거리두기 3/study-cafe-keeping-distance-3.java
@@ -10,41 +10,28 @@ public class Main {
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         int n = Integer.parseInt(st.nextToken());
-
         st = new StringTokenizer(br.readLine());
-        String s = st.nextToken();
+        String str = st.nextToken();
+        boolean[] humans = new boolean[n];
 
-        int answer = 0;
         for (int i = 0; i < n; i++) {
-            for (int j = i + 1; j < n; j++) {
-
-                if (s.charAt(i) == '0' && s.charAt(j) == '0') {
-                    int minDistance = Integer.MAX_VALUE;
-                    String str = s.substring(0, i) + "1" + s.substring(i + 1, j) + "1" + s.substring(j + 1);
-                    int first = str.indexOf("1");
-
-                    // 처음부터 끝까지 떨어진 사람들의 거리를 구한다.
-                    int distance = 0;
-                    for (int k = 0; k < n; k++) {
-                        // distance == 0인 경우는 붙어 있다.
-                        if (str.charAt(k) == '1') {
-                            if (k == first) {
-                                distance = 0;
-                                continue;
-                            }
-                            minDistance = Math.min(minDistance, distance);
-                            distance = 0;
-                        } else {
-                            distance++;
-                        }
-                    }
-
-                    answer = Math.max(answer, minDistance);
-                }
-
+            if (str.charAt(i) == '1') {
+                humans[i] = true;
             }
         }
 
-        System.out.println(answer + 1);
+        int maxDistance = 0;
+        for (int i = 0; i < humans.length; i++) {
+            if (humans[i]) {
+                for (int j = i + 1; j < humans.length; j++) {
+                    if (humans[j]) {
+                        maxDistance = Math.max(maxDistance, j - i);
+                        break;
+                    }
+                }
+            }
+        }
+
+        System.out.println(maxDistance / 2);
     }
 }
