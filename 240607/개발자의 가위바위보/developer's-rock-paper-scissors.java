20d1e03c149a507eb2f08c9be11eb057be9bbc27
Icksoo <87226112+dlrtn@users.noreply.github.com>
2024-06-07 15:01:56 +0900

    Title: 개발자의 가위바위보, Time: 113ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240607/개발자의 가위바위보/developer's-rock-paper-scissors.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int[][] arr = new int[3][3];
+
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+
+            arr[x - 1][y - 1]++;
+        }
+
+        System.out.println(Math.max(arr[0][1] + arr[2][0] + arr[1][2], arr[1][0] + arr[0][2] + arr[2][1]));
+    }
+}
