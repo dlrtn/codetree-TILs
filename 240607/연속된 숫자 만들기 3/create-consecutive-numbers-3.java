0785dd572b54df46867b01029086d5731d65485d
Icksoo <87226112+dlrtn@users.noreply.github.com>
2024-06-07 15:03:54 +0900

    Title: 연속된 숫자 만들기 3, Time: 95ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240607/연속된 숫자 만들기 3/create-consecutive-numbers-3.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int x = Integer.parseInt(st.nextToken());
+        int y = Integer.parseInt(st.nextToken());
+        int z = Integer.parseInt(st.nextToken());
+
+        int answer = Math.max(Math.abs(x - y), Math.abs(y - z));
+
+        System.out.println(answer - 1);
+    }
+}
