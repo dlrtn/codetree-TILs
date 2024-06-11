e6d923f554228809f74bc96ef2a97e74a14deca4
Icksoo <87226112+dlrtn@users.noreply.github.com>
2024-06-11 14:07:28 +0900

    Title: 최소 와이파이 수, Time: 88ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240611/최소 와이파이 수/minimum-number-of-wifi.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static int count = 0;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[n];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int oneCount = 0;
+        for (int i = 0; i < n; i++) {
+            if (arr[i] == 1) {
+                oneCount++;
+            } else {
+                oneCount = 0;
+            }
+
+            if (oneCount == m * 2 - 1) {
+                count++;
+            }
+        }
+
+        System.out.println(count);
+    }
+}
