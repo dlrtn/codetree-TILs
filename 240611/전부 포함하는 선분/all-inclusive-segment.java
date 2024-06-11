219a54697663c22401f3cd894bb8be74353858bd
Icksoo <87226112+dlrtn@users.noreply.github.com>
2024-06-11 15:41:08 +0900

    Title: 전부 포함하는 선분, Time: 224ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240611/전부 포함하는 선분/all-inclusive-segment.java
@@ -0,0 +1,60 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static int count = 0;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        List<Line> lines = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            int startX = Integer.parseInt(st.nextToken());
+            int endX = Integer.parseInt(st.nextToken());
+
+            lines.add(new Line(startX, endX));
+        }
+
+        lines.sort((a, b) -> {
+            if (a.startX == b.startX) {
+                return a.endX - b.endX;
+            }
+            return a.startX - b.startX;
+        });
+
+        if (n > 2) {
+            if (lines.get(0).endX - lines.get(1).startX < lines.get(n - 1).endX - lines.get(
+                    n - 2).startX) {
+                // 뒤에 선분을 제외하는 경우,
+                System.out.println(Math.abs(lines.get(0).startX - lines.get(n - 2).endX));
+            } else {
+                System.out.println(Math.abs(lines.get(1).startX - lines.get(n - 1).endX));
+            }
+        } else {
+            System.out.println(Math.abs(lines.get(0).startX - lines.get(1).endX));
+        }
+    }
+
+    public static class Line {
+
+        int startX;
+        int endX;
+
+        public void print() {
+            System.out.println(startX + " " + endX);
+        }
+
+        public Line(int startX, int endX) {
+            this.startX = startX;
+            this.endX = endX;
+        }
+    }
+}
