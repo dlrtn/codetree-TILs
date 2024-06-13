408db5ea99f3841f364c4a92721716d7a5f7c335
Icksoo <87226112+dlrtn@users.noreply.github.com>
2024-06-13 11:38:21 +0900

    Title: 황금비율 토스트, Time: 1061ms, Memory: 36MB, Status: Passed - CodeTree

--- 240613/황금비율 토스트/golden-toast.java
+++ 240613/황금비율 토스트/golden-toast.java
@@ -1,60 +1,52 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.Scanner;
 import java.util.LinkedList;
 import java.util.ListIterator;
-import java.util.StringTokenizer;
-import java.util.stream.Collectors;
 
 public class Main {
+    public static int n, m;
+    public static String s;
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
+        // 입력:
+        n = sc.nextInt();
+        m = sc.nextInt();
+        s = sc.next();
 
-        st = new StringTokenizer(br.readLine());
-        String str = st.nextToken();
-        LinkedList<Character> s = new LinkedList<>();
-        s.addAll(str.chars().mapToObj(e -> (char) e).collect(Collectors.toCollection(LinkedList::new)));
+        // 연결리스트 정의
+        LinkedList<Character> l = new LinkedList<>();
+        for(int i = 0; i < s.length(); i++)
+			l.add(s.charAt(i));
+        // Iterator 정의 (맨 끝 위치에서 시작하기 위해서는 listIterator안에 l.size() 필요)
+        ListIterator<Character> it = l.listIterator(l.size());
 
-        ListIterator<Character> it = s.listIterator(s.size());
+        while(m-- > 0) {
+            char command = sc.next().charAt(0);
 
-        for (int i = 0; i < m; i++) {
-            st = new StringTokenizer(br.readLine());
-
-            String instruction = st.nextToken();
-            switch (instruction) {
-                case "L":
-                    if (it.hasPrevious()) {
-                        it.previous();
-                    }
-                    break;
-                case "R":
-                    if (it.hasNext()) {
-                        it.next();
-                    }
-                    break;
-                case "D":
-                    if (it.hasNext()) {
-                        it.next();
-                        it.remove();
-                    }
-                    break;
-                case "P":
-                    String c = st.nextToken();
-                    it.add(c.charAt(0));
-                    break;
+            if(command == 'L') {
+                if(it.hasPrevious()) // 빵들의 맨 앞이 아니라면
+                    it.previous();   // 앞으로 이동합니다.
+            } 
+            else if(command == 'R') {
+                if(it.hasNext())     // 빵들의 맨 뒤가 아니라면
+                    it.next();       // 뒤로 이동합니다.
             }
+            else if(command == 'D') {
+                if(it.hasNext()) {   // 빵들의 맨 뒤가 아니라면
+                    it.next();
+                    it.remove();     // 바로 뒤에 있는 빵을 제거합니다.
+                }
+            }
+            else if(command == 'P') {
+                char c = sc.next().charAt(0);
+                it.add(c);           // 가리키는 위치에 문자 c를 추가합니다.
+            }    
         }
 
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < s.size(); i++) {
-            sb.append(s.get(i));
-        }
-
-        System.out.println(sb);
+        // 출력:
+        it = l.listIterator();
+        while(it.hasNext())
+            System.out.print(it.next());
     }
 }
