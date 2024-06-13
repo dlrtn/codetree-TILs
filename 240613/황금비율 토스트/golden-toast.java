cd5d3e0d893177f03fc9b9639e9362df8482a651
Icksoo <87226112+dlrtn@users.noreply.github.com>
2024-06-13 11:23:18 +0900

    Title: 황금비율 토스트, Time: 3011ms, Memory: 23MB, Status: Time Limit Exceed - CodeTree

---
+++ 240613/황금비율 토스트/golden-toast.java
@@ -0,0 +1,59 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.ListIterator;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        LinkedList<Character> s = new LinkedList<>();
+        st = new StringTokenizer(br.readLine());
+        String str = st.nextToken();
+
+        for (int i = 0; i < n; i++) {
+            s.add(str.charAt(i));
+        }
+
+        ListIterator<Character> it = s.listIterator(s.size());
+
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            String instruction = st.nextToken();
+            switch (instruction) {
+                case "L":
+                    if (it.hasPrevious()) {
+                        it.previous();
+                    }
+                    break;
+                case "R":
+                    if (it.hasNext()) {
+                        it.next();
+                    }
+                    break;
+                case "D":
+                    if (it.hasNext()) {
+                        it.next();
+                        it.remove();
+                    }
+                    break;
+                case "P":
+                    String c = st.nextToken();
+                    it.add(c.charAt(0));
+                    break;
+            }
+        }
+
+        for (int i = 0; i < s.size(); i++) {
+            System.out.print(s.get(i));
+        }
+    }
+}
