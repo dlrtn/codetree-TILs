e72e558ded05a6804fe46cf9474b41ebb131ebd7
Icksoo <87226112+dlrtn@users.noreply.github.com>
2024-06-23 22:15:47 +0900

    Title: 원형 순열에서의 인원 제거, Time: 446ms, Memory: 78MB, Status: Passed - CodeTree

---
+++ 240623/원형 순열에서의 인원 제거/josephus-permutations.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        Queue<Integer> queue = new LinkedList<>();
+
+        for (int i = 1; i <= n; i++) {
+            queue.add(i);
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < k - 1; j++) {
+                queue.add(queue.poll());
+            }
+
+            System.out.print(queue.poll() + " ");
+
+        }
+    }
+}
