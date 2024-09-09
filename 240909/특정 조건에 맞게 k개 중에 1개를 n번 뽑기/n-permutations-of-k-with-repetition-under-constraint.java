a5505ab958ef42c86e0d278c8dbdd51eac09ab80
Icksoo <87226112+dlrtn@users.noreply.github.com>
2024-09-09 17:20:52 +0900

    Title: 특정 조건에 맞게 k개 중에 1개를 n번 뽑기, Time: 235ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240909/특정 조건에 맞게 k개 중에 1개를 n번 뽑기/n-permutations-of-k-with-repetition-under-constraint.java
@@ -0,0 +1,42 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+
+public class Main {
+
+    public static ArrayList<Integer> arrayList = new ArrayList<>();
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int k = Integer.parseInt(st.nextToken());
+        int n = Integer.parseInt(st.nextToken());
+
+        backtracking(k, n, 0);
+    }
+
+    public static void backtracking(int k, int n, int depth) {
+        if (depth == n) {
+            if (arrayList.stream().max(Integer::compareTo).get() == arrayList.stream().min(Integer::compareTo).get()) {
+                return;
+            }
+
+            arrayList.forEach((item) -> {
+                System.out.print(item + " ");
+            });
+            System.out.println();
+            return;
+        }
+
+        for (int i = 1; i <= k; i++) {
+            arrayList.add(i);
+            backtracking(k, n, depth + 1);
+            arrayList.remove(arrayList.size() - 1);
+        }
+    }
+}
