f2e95c8423e3dd64feb194347ee400a2e59f8cc9
Icksoo <87226112+dlrtn@users.noreply.github.com>
2024-09-12 13:42:01 +0900

    Title: xor 결과 최대 만들기, Time: 3020ms, Memory: 11MB, Status: Time Limit Exceed - CodeTree

---
+++ 240912/xor 결과 최대 만들기/max-of-xor.java
@@ -0,0 +1,55 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+
+public class Main {
+
+    public static ArrayList<Integer> arrayList = new ArrayList<>();
+
+    public static int[] arr;
+
+    public static int answer = Integer.MIN_VALUE;
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        arr = new int[n];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        backtracking(m, n, 0);
+
+        System.out.println(answer);
+    }
+
+    public static void backtracking(int m, int n, int depth) {
+        if (depth == m) {
+            int num = 0;
+            for (int i = 0; i < m; i++) {
+                num ^= arrayList.get(i);
+            }
+
+            answer = Math.max(answer, num);
+            return;
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (!arrayList.contains(arr[i])) {
+                arrayList.add(arr[i]);
+                backtracking(m, n, depth + 1);
+                arrayList.remove(arrayList.size() - 1);
+            }
+        }
+    }
+}
