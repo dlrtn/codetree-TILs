da8a54f466baa778bfe941504761af74bf927a05
Icksoo <87226112+dlrtn@users.noreply.github.com>
2024-09-26 17:40:23 +0900

    Title: 사각형 채우기 3, Time: 102ms, Memory: 8MB, Status: Wrong Answer - CodeTree

--- 240926/사각형 채우기 3/rectangle-fill-3.java
+++ 240926/사각형 채우기 3/rectangle-fill-3.java
@@ -1,32 +1,39 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.StringTokenizer;
+
 
 public class Main {
-    public static final int MOD = 1000000007;
-    public static final int MAX_N = 1000;
-    
-    // 변수 선언
-    public static int n;
-    
-    public static long[] dp = new long[MAX_N + 1];
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        // 입력:
-        n = sc.nextInt();
+    public static int count = 0;
 
-        // 초기 조건 설정
+    public static int answer = 0;
+
+    public static Map<Integer, Integer> map = new HashMap<>();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+
+        int[] dp = new int[1001];
+
         dp[0] = 1;
         dp[1] = 2;
 
-        // 점화식에 따라 dp값 채우기
-        // dp[i] = dp[i - 1] * 2 + dp[i - 2] * 3 +
-        //         (dp[i - 3] + dp[i - 4] + dp[i - 5] + ... dp[0]) * 2
-        for(int i = 2; i <= n; i++) {
-            dp[i] = (dp[i - 1] * 2 + dp[i - 2] * 3) % MOD;
-            for(int j = i - 3; j >= 0; j--)
-                dp[i] = (dp[i] + dp[j] * 2) % MOD;
+        for (int i = 2; i <= 1000; i++) {
+            dp[i] = (2 * dp[i - 1] + 3 * dp[i - 2]) % 1000000007;
+
+            for (int j = i - 3; j >= 0; j--) {
+                dp[i] += (dp[j] * 2) % 1000000007;
+            }
         }
-        
-        System.out.print(dp[n]);
+
+        System.out.println(dp[n]);
     }
 }
