32600d57a71d533fbb5dfa0bb3f8b1d0b7bbdf7e
Icksoo <87226112+dlrtn@users.noreply.github.com>
2024-09-26 15:12:26 +0900

    Title: 계단 오르기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240926/계단 오르기/climbing-stairs.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.StringTokenizer;
+
+
+public class Main {
+
+    public static int count = 0;
+
+    public static int answer = 0;
+
+    public static Map<Integer, Integer> map = new HashMap<>();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+
+        int[] dp = new int[46];
+
+        dp[0] = 0;
+        dp[1] = 0;
+        dp[2] = 1;
+        dp[3] = 1;
+        dp[4] = 1;
+        dp[5] = 2;
+
+
+        for (int i = 6; i <= 1000; i++) {
+            dp[i] = dp[i - 2] + dp[i - 3];
+        }
+
+        System.out.println(dp[n]);
+    }
+}
