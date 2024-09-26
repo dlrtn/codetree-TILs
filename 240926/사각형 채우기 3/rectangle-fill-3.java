b4a215d58dad8633430add083b14aec2d73e4b44
Icksoo <87226112+dlrtn@users.noreply.github.com>
2024-09-26 17:34:51 +0900

    Title: 사각형 채우기 3, Time: 138ms, Memory: 10MB, Status: Passed - CodeTree

--- 240926/사각형 채우기 3/rectangle-fill-3.java
+++ 240926/사각형 채우기 3/rectangle-fill-3.java
@@ -1,37 +1,32 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.Map;
-import java.util.StringTokenizer;
-
+import java.util.Scanner;
 
 public class Main {
+    public static final int MOD = 1000000007;
+    public static final int MAX_N = 1000;
+    
+    // 변수 선언
+    public static int n;
+    
+    public static long[] dp = new long[MAX_N + 1];
 
-    public static int count = 0;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // 입력:
+        n = sc.nextInt();
 
-    public static int answer = 0;
-
-    public static Map<Integer, Integer> map = new HashMap<>();
-
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-
-        int n = Integer.parseInt(st.nextToken());
-
-        int[] dp = new int[1001];
-
-        dp[0] = 0;
+        // 초기 조건 설정
+        dp[0] = 1;
         dp[1] = 2;
-        dp[2] = 7;
 
-
-        for (int i = 3; i <= 1000; i++) {
-            dp[i] = (2 * dp[i - 1] + 4 * dp[i - 2]) % 1000000007;
+        // 점화식에 따라 dp값 채우기
+        // dp[i] = dp[i - 1] * 2 + dp[i - 2] * 3 +
+        //         (dp[i - 3] + dp[i - 4] + dp[i - 5] + ... dp[0]) * 2
+        for(int i = 2; i <= n; i++) {
+            dp[i] = (dp[i - 1] * 2 + dp[i - 2] * 3) % MOD;
+            for(int j = i - 3; j >= 0; j--)
+                dp[i] = (dp[i] + dp[j] * 2) % MOD;
         }
-
-        System.out.println(dp[n]);
+        
+        System.out.print(dp[n]);
     }
 }
