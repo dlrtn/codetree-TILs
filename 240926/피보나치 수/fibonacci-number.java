43e60d33b6a0320739106dd1e03509ed618cd1d2
Icksoo <87226112+dlrtn@users.noreply.github.com>
2024-09-26 15:04:18 +0900

    Title: 피보나치 수, Time: 90ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240926/피보나치 수/fibonacci-number.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.StringTokenizer;
+
+
+public class Main {
+
+    public static int count = 0;
+
+    public static int answer = 0;
+
+    public static Map<Integer, Integer> map = new HashMap<>();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+
+        int[] dp = new int[46];
+
+        dp[0] = 0;
+        dp[1] = 1;
+
+        for (int i = 2; i < 45; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+
+        System.out.println(dp[n]);
+    }
+}
