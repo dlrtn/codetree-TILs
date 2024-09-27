b37433a818754b20c01839bd653d5c6d7ef36ce8
Icksoo <87226112+dlrtn@users.noreply.github.com>
2024-09-27 17:08:53 +0900

    Title: 정수 사각형 최장 증가 수열, Time: 1522ms, Memory: 83MB, Status: Passed - CodeTree

--- 240927/정수 사각형 최장 증가 수열/lis-on-the-integer-grid.java
+++ 240927/정수 사각형 최장 증가 수열/lis-on-the-integer-grid.java
@@ -1,106 +1,84 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.Map;
-import java.util.StringTokenizer;
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Collections;
 
+class Cell implements Comparable<Cell> {
+    int num, x, y;
+
+    public Cell(int num, int x, int y) {
+        this.num = num;
+        this.x = x;
+        this.y = y;
+    }
+
+    @Override
+    public int compareTo(Cell c) {
+        return this.num - c.num;      // num 기준으로 오름차순 정렬합니다.
+    }
+}
 
 public class Main {
-
+    public static final int DIR_NUM = 4;
+    public static final int MAX_N = 500;
+    
+    // 변수 선언
     public static int n;
-    public static int[] dx = {0, 0, 1, -1};
-    public static int[] dy = {1, -1, 0, 0};
-    public static int[][] dp;
-    public static int[][] arr;
+    public static int[][] grid = new int[MAX_N][MAX_N];
+    public static int[][] dp = new int[MAX_N][MAX_N];
+    
+    public static ArrayList<Cell> cells = new ArrayList<>();
+    public static int ans = 0;
+    
+    public static boolean inRange(int x, int y) {
+        return 0 <= x && x < n && 0 <= y && y < n;
+    }
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // 입력
+        n = sc.nextInt();
 
-        n = Integer.parseInt(st.nextToken());
+        for(int i = 0; i < n; i++)
+            for(int j = 0; j < n; j++)
+                grid[i][j] = sc.nextInt();
 
-        dp = new int[n][n];
-        arr = new int[n][n];
-
-        for (int i = 0; i < n; i++) {
-            st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < n; j++) {
-                arr[i][j] = Integer.parseInt(st.nextToken());
-            }
-        }
-
-        int min = Integer.MAX_VALUE;
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                min = Integer.min(arr[i][j], min);
-            }
-        }
-
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (arr[i][j] == min) {
-                    dp[i][j] = 1;
-                } else {
-                    dp[i][j] = -1;
-                }
-            }
-        }
-
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (dp[i][j] == 1) {
-                    dfs(i, j);
-                }
-            }
-        }
-
-        int max = 0;
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                max = Integer.max(max, dp[i][j]);
-            }
-        }
+        // 각 칸에 적혀있는 정수값 기준으로
+        // 오름차순이 되도록 칸의 위치들을 정렬합니다.
+        // 편하게 정렬하기 위해
+        // (칸에 적혀있는 값, 행 위치, 열 위치) 순으로 넣어줍니다.
+        for(int i = 0; i < n; i++)
+            for(int j = 0; j < n; j++)
+                cells.add(new Cell(grid[i][j], i, j));
         
-        System.out.println(max);
-    }
+        // 오름차순으로 정렬을 진행합니다.
+        Collections.sort(cells);
 
-    public static void dfs(int x, int y) {
-        if (dp[x][y] != -1) {
-            dp[x][y] = 1;
-        } else {
-            int max = 0;
-            for (int i = 0; i < 4; i++) {
-                if (x + dx[i] >= 0 && y + dy[i] >= 0 && x + dx[i] < n && y + dy[i] < n && arr[x + dx[i]][y + dy[i]] < arr[x][y]) {
-                    max = Integer.max(max, dp[x + dx[i]][y + dy[i]]);
-                }
-            }
+        // 처음 DP 값들은 전부 1로 초기화해줍니다. (해당 칸에서 시작하는 경우)
+        for(int i = 0; i < n; i++)
+            for(int j = 0; j < n; j++)
+                dp[i][j] = 1;
 
-            dp[x][y] = max + 1;
-        }
+        // 정수값이 작은 칸부터 순서대로 보며
+        // 4방향에 대해 dp 값을 갱신해줍니다.
+        for(int i = 0; i < cells.size(); i++) {
+            int x = cells.get(i).x;
+            int y = cells.get(i).y;
+            int[] dx = new int[]{-1, 1,  0, 0};
+            int[] dy = new int[]{ 0, 0, -1, 1};
 
-        for (int i = 0; i < 4; i++) {
-            int nx = x + dx[i];
-            int ny = y + dy[i];
-
-            if (nx >= 0 && ny >= 0 && nx < n && ny < n) {
-                if (dp[nx][ny] == -1 && arr[nx][ny] > arr[x][y]) {
-                    dfs(nx, ny);
-                }
+            // 인접한 4개의 칸에 대해 갱신을 진행합니다.
+            for(int j = 0; j < DIR_NUM; j++) {
+                int nx = x + dx[j], ny = y + dy[j];
+                if(inRange(nx, ny) && grid[nx][ny] > grid[x][y])
+                    dp[nx][ny] = Math.max(dp[nx][ny], dp[x][y] + 1);
             }
         }
 
+        // 전체 수들 중 최댓값을 찾습니다.
+        for(int i = 0; i < n; i++)
+            for(int j = 0; j < n; j++)
+                ans = Math.max(ans, dp[i][j]);
 
-    }
-
-    public static class Pair {
-        int x;
-        int y;
-
-        public Pair(int x, int y) {
-            this.x = x;
-            this.y = y;
-        }
+        System.out.print(ans);
     }
 }
