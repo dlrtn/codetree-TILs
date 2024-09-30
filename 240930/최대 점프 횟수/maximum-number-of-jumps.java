c7b056d8c464dd6ce2139a0f1eee549f5712f55c
Icksoo <87226112+dlrtn@users.noreply.github.com>
2024-09-30 17:52:34 +0900

    Title: 최대 점프 횟수, Time: 131ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 240930/최대 점프 횟수/maximum-number-of-jumps.java
@@ -0,0 +1,44 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[n];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int[] dp = new int[n];
+        for (int i = 0; i < n; i++) {
+            dp[i] = Integer.MIN_VALUE;
+        }
+        dp[0] = 0;
+
+        for (int i = 1; i < n; i++) {
+            int max = 0;
+            for (int j = 0; j < i; j++) {
+                if (arr[j] >= i - j && (dp[j] != 0 || j == 0)) {
+                    max = Math.max(max, dp[j] + 1);
+                }
+            }
+            dp[i] = max;
+        }
+
+        int answer = 0;
+        for (int i = 0; i < n; i++) {
+            answer = Math.max(answer, dp[i]);
+        }
+
+        System.out.println(answer);
+    }
+}
