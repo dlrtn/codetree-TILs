b61fdfe85cf374cf62616429b898cd7b17ce3ecb
Icksoo <87226112+dlrtn@users.noreply.github.com>
2024-10-03 01:11:40 +0900

    Title: 계단 오르기 2, Time: 107ms, Memory: 9MB, Status: Wrong Answer - CodeTree

---
+++ 241003/계단 오르기 2/climbing-stairs-2.java
@@ -0,0 +1,48 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+
+        int[] ints = new int[n];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            ints[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int[][] dp = new int[n + 1][3];
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < 3; j++) {
+                dp[i][j] = Integer.MIN_VALUE;
+            }
+        }
+
+        dp[0][0] = 0;
+        dp[1][1] = ints[0];
+        dp[2][0] = ints[1];
+        for (int i = 2; i <= n; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (j - 1 < 0) {
+                    continue;
+                }
+                dp[i][j] = Math.max(dp[i - 1][j - 1], dp[i - 2][j]) + ints[i - 1];
+            }
+        }
+
+        int max = 0;
+        for (int i = 0; i <= n; i++) {
+            for (int j = 0; j < 3; j++) {
+                max = Math.max(max, dp[i][j]);
+            }
+        }
+        
+        System.out.println(max);
+    }
+}
