af2cb0729b53af06e80bacfff537de5fb66ea5e9
Icksoo <87226112+dlrtn@users.noreply.github.com>
2024-10-17 16:34:27 +0900

    Title: 최장 공통 부분 수열, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 241017/최장 공통 부분 수열/longest-common-sequence.java
+++ 241017/최장 공통 부분 수열/longest-common-sequence.java
@@ -13,19 +13,23 @@ public class Main {
         st = new StringTokenizer(br.readLine());
         String b = st.nextToken();
 
-        int[][] dp = new int[a.length() + 1][b.length() + 1];
+        int[][] dp = new int[a.length()][b.length()];
         dp[0][0] = a.charAt(0) == b.charAt(0) ? 1 : 0;
 
         for (int i = 0; i < a.length(); i++) {
             for (int j = 0; j < b.length(); j++) {
-                if (i == 0 || j == 0) {
-                    dp[i][j] = 0;
-                } else {
-                    if (a.charAt(i) == b.charAt(j)) {
-                        dp[i][j] = dp[i - 1][j - 1] + 1;
-                    } else {
-                        dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                if (a.charAt(i) == b.charAt(j)) {
+                    if (i == 0 || j == 0) {
+                        dp[i][j] = 1;
+                        continue;
                     }
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    if (i == 0 && j == 0) {
+                        dp[i][j] = 0;
+                        continue;
+                    }
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
                 }
             }
         }
