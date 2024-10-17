6aedbf22c3e9b811d628e69a3964ebbc24b1934d
Icksoo <87226112+dlrtn@users.noreply.github.com>
2024-10-17 16:32:45 +0900

    Title: 최장 공통 부분 수열, Time: 94ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 241017/최장 공통 부분 수열/longest-common-sequence.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        String a = st.nextToken();
+
+        st = new StringTokenizer(br.readLine());
+        String b = st.nextToken();
+
+        int[][] dp = new int[a.length() + 1][b.length() + 1];
+        dp[0][0] = a.charAt(0) == b.charAt(0) ? 1 : 0;
+
+        for (int i = 0; i < a.length(); i++) {
+            for (int j = 0; j < b.length(); j++) {
+                if (i == 0 || j == 0) {
+                    dp[i][j] = 0;
+                } else {
+                    if (a.charAt(i) == b.charAt(j)) {
+                        dp[i][j] = dp[i - 1][j - 1] + 1;
+                    } else {
+                        dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                    }
+                }
+            }
+        }
+
+        System.out.println(dp[a.length() - 1][b.length() - 1]);
+
+    }
+}
