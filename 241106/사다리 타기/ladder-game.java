096c1d2885601a74e2ad6d209eced6b41e166cab
Icksoo <87226112+dlrtn@users.noreply.github.com>
2024-11-06 17:15:15 +0900

    Title: 사다리 타기, Time: 451ms, Memory: 16MB, Status: Passed - CodeTree

--- 241106/사다리 타기/ladder-game.java
+++ 241106/사다리 타기/ladder-game.java
@@ -5,12 +5,15 @@ import java.util.LinkedList;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int[] result;
     static int n;
     static int m;
     static boolean[] used;
     static int answer = Integer.MAX_VALUE;
     static int[] firstResult;
+    static int lastHeight;
+    static int allCount = 0;
+    static LinkedList<Integer> useList = new LinkedList<>();
+    static LinkedList<Ladder> ladders = new LinkedList<>();
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -19,85 +22,66 @@ public class Main {
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
 
-        result = new int[n];
-        used = new boolean[m];
-        for (int i = 0; i < n; i++) {
-            result[i] = i + 1;
-        }
-
-        LinkedList<Sadari> sadari = new LinkedList<>();
         for (int i = 0; i < m; i++) {
             st = new StringTokenizer(br.readLine());
 
             int x = Integer.parseInt(st.nextToken());
             int height = Integer.parseInt(st.nextToken());
 
-            sadari.add(new Sadari(x, height));
+            ladders.add(new Ladder(x, height));
+            useList.add(i);
         }
+        ladders.sort((o1, o2) -> {
+            if (o1.height == o2.height) {
+                return o1.x - o2.x;
+            }
+            return o1.height - o2.height;
+        });
+        lastHeight = ladders.getLast().height;
 
-        used = new boolean[m];
-        for (int i = 0; i < m; i++) {
-            used[i] = true;
-        }
-        firstResult = sadari(sadari);
-        for (int i = 0; i < m; i++) {
-            used[i] = false;
-        }
+        firstResult = sadari();
 
-        backtracking(sadari, 0);
+        useList.clear();
+        backtracking(0);
 
         System.out.println(answer);
     }
 
-    public static void backtracking(LinkedList<Sadari> sadari, int depth) {
-        int count = 0;
-        for (int i = 0; i < m; i++) {
-            if (used[i]) {
-                count++;
-            }
-        }
-
+    public static void backtracking(int depth) {
         int answerCount = 0;
-        int[] temp = sadari(sadari);
+        int[] temp = sadari();
         for (int i = 0; i < n; i++) {
             if (temp[i] == firstResult[i]) {
                 answerCount++;
             }
         }
         if (answerCount == n) {
-            answer = Math.min(answer, count);
+            answer = Math.min(answer, useList.size());
         }
 
         for (int i = depth; i < m; i++) {
-            if (!used[i]) {
-                used[i] = true;
+            if (useList.contains(i)) {
+                continue;
             }
-            backtracking(sadari, depth + 1);
-            used[i] = false;
+            useList.add(i);
+            backtracking(i + 1);
+            useList.removeLast();
         }
     }
 
-    public static int[] sadari(LinkedList<Sadari> sadaris) {
+    public static int[] sadari() {
         int[] temp = new int[n];
 
-        sadaris.sort((o1, o2) -> {
-            if (o1.height == o2.height) {
-                return o1.x - o2.x;
-            }
-            return o1.height - o2.height;
-        });
-
-        int lastHeight = sadaris.getLast().height;
         for (int i = 0; i < n; i++) {
             int nowX = i + 1;
 
             for (int j = 1; j <= lastHeight; j++) {
-                for (int k = 0; k < m; k++) {
-                    Sadari now = sadaris.get(k);
-                    if (used[k] && (nowX) == now.x && j == now.height) {
+                for (int k = 0; k < useList.size(); k++) {
+                    Ladder now = ladders.get(useList.get(k));
+                    if (nowX == now.x && j == now.height) {
                         nowX++;
                         break;
-                    } else if (used[k] && (nowX - 1) == now.x && j == now.height) {
+                    } else if ((nowX - 1) == now.x && j == now.height) {
                         nowX--;
                         break;
                     }
@@ -110,11 +94,11 @@ public class Main {
         return temp;
     }
 
-    public static class Sadari {
+    public static class Ladder {
         int x;
         int height;
 
-        public Sadari(int x, int height) {
+        public Ladder(int x, int height) {
             this.x = x;
             this.height = height;
         }
