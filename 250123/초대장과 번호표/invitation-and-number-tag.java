86d185600f9eba814719794e3a180cf66bcac5cb
Icksoo <87226112+dlrtn@users.noreply.github.com>
2025-01-23 14:52:13 +0900

    Title: 초대장과 번호표, Time: 1408ms, Memory: 108MB, Status: Passed - CodeTree

--- 250123/초대장과 번호표/invitation-and-number-tag.java
+++ 250123/초대장과 번호표/invitation-and-number-tag.java
@@ -1,62 +1,62 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.Scanner;
 import java.util.HashSet;
-import java.util.LinkedList;
+import java.util.ArrayList;
 import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.LinkedList;
 
 public class Main {
+    public static final int MAX_G = 250000;
+    public static final int MAX_N = 100000;
+    
+    public static int n, g;
+    public static boolean[] invited = new boolean[MAX_N];
+    public static HashSet<Integer>[] groups = new HashSet[MAX_G];
+    public static ArrayList<Integer>[] peopleGroups = new ArrayList[MAX_N];
+    public static Queue<Integer> q = new LinkedList<>();
+    public static int ans;
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // 입력:
+        n = sc.nextInt();
+        g = sc.nextInt();
 
-        int n = Integer.parseInt(st.nextToken());
-        int g = Integer.parseInt(st.nextToken());
+        for(int i = 0; i < n; i++)
+            peopleGroups[i] = new ArrayList<>();
 
-        HashSet<Integer> invited = new HashSet<>();
-        Queue<HashSet<Integer>> groups = new LinkedList<>();
-        invited.add(Integer.valueOf(1));
-        for (int i = 0; i < g; i++) {
-            st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < g; i++)
+            groups[i] = new HashSet<>();
 
-            int groupSize = Integer.parseInt(st.nextToken());
-
-            HashSet<Integer> group = new HashSet<>();
-
-            for (int j = 0; j < groupSize; j++) {
-                int member = Integer.parseInt(st.nextToken());
-                group.add(Integer.valueOf(member));
-
-                groups.add(group);
+        for(int i = 0; i < g; i++) {
+            int s = sc.nextInt();
+            for(int j = 0; j < s; j++) {
+                int x = sc.nextInt(); x--;
+                groups[i].add(x);
+                peopleGroups[x].add(i);
             }
         }
 
-        int count = 0;
-        while (!groups.isEmpty()) {
-            HashSet<Integer> group = groups.poll();
-            HashSet<Integer> invitedMemberInGroup = new HashSet<>();
+        q.add(0);
+        invited[0] = true;
+        while(!q.isEmpty()) {
+            int x = q.poll();
+            ans++;
 
-            for (Integer invitedMember : invited) {
-                if (group.contains(invitedMember)) {
-                    invitedMemberInGroup.add(invitedMember);
+            for(int i = 0; i < peopleGroups[x].size(); i++) {
+                int gNum = peopleGroups[x].get(i);
+
+                groups[gNum].remove(x);
+                
+                if(groups[gNum].size() == 1) {
+                    int pNum = new ArrayList<>(groups[gNum]).get(0);
+                    if(!invited[pNum]) {
+                        invited[pNum] = true;
+                        q.add(pNum);
+                    }
                 }
             }
-
-            if (invitedMemberInGroup.size() == group.size() - 1) {
-                invited.addAll(group);
-                count = 0;
-            } else {
-                groups.add(group);
-                count++;
-            }
-
-            if (count == groups.size()) {
-                break;
-            }
         }
 
-        System.out.println(invited.size());
+        System.out.print(ans);
     }
 }
