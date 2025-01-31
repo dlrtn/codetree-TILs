d771c319834a6805d84172a42937985202aa7890
Icksoo <87226112+dlrtn@users.noreply.github.com>
2025-01-31 11:09:50 +0900

    Title: top k 숫자, Time: 518ms, Memory: 22MB, Status: Passed - CodeTree

---
+++ 250131/top k 숫자/top-k-elements.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Comparator;
+import java.util.StringTokenizer;
+import java.util.TreeSet;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        TreeSet<Integer> set = new TreeSet<>(new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                return o2 - o1;
+            }
+        });
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            set.add(Integer.parseInt(st.nextToken()));
+        }
+
+        for (int i = 0; i < k; i++) {
+            System.out.print(set.pollFirst() + " ");
+        }
+    }
+}
