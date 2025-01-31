e056f39032f3f541ab38cc1a2d016028c68995b9
Icksoo <87226112+dlrtn@users.noreply.github.com>
2025-01-31 11:59:56 +0900

    Title: 자리 차지하기, Time: 942ms, Memory: 35MB, Status: Passed - CodeTree

--- 250131/자리 차지하기/take-place.java
+++ 250131/자리 차지하기/take-place.java
@@ -1,46 +1,49 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.Scanner;
+import java.util.TreeSet;
 
 public class Main {
+    public static final int MAX_N = 100000;
+    
+    // 변수 선언
+    public static int n, m;
+    public static int[] arr = new int[MAX_N];
+    
+    public static TreeSet<Integer> seats = new TreeSet<>();
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // 입력:
+        n = sc.nextInt();
+        m = sc.nextInt();
+        for(int i = 0; i < n; i++)
+            arr[i] = sc.nextInt();
+        
+        // 1번부터 m번까지 전부 비어져 있으므로
+        // 빈 위치를 treeset으로 관리합니다.
+        for(int i = 1; i <= m; i++)
+            seats.add(i);
 
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
+        // 답을 구해줍니다.
+        int ans = 0;
 
-        boolean[] check = new boolean[m + 1];
-
-        st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < n; i++) {
-            int num = Integer.parseInt(st.nextToken());
-
-            if (!arrange(check, num, m)) {
+        // 순서대로 앉혀봅니다.
+        // 최선의 자리 선택은
+        // ai보다 같거나 작은 최대 위치에 자리배치를 하는 것입니다.
+        for(int i = 0; i < n; i++) {
+            // ai보다 같거나 작은 최댓값을 찾습니다.
+            // 존재하는 경우
+            // 해당 위치에 사람을 앉혀줍니다.
+            if(seats.floor(arr[i]) != null) {
+                int num = seats.floor(arr[i]);
+                seats.remove(num);
+                
+                // 답을 갱신해줍니다.
+                ans++;
+            }
+            else
                 break;
-            }
         }
 
-        int count = 0;
-        for (int i = 1; i <= m; i++) {
-            if (check[i]) {
-                count++;
-            }
-        }
-
-        System.out.println(count);
-    }
-
-    public static boolean arrange(boolean[] check, int num, int m) {
-        for (int i = num; i >= 1; i--) {
-            if (!check[i]) {
-                check[i] = true;
-
-                return true;
-            }
-        }
-        return false;
+        System.out.print(ans);
     }
 }
