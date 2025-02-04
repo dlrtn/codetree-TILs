085ccafe6304bc64a820aecf0cc3637e5fcfee40
Icksoo <87226112+dlrtn@users.noreply.github.com>
2025-02-04 11:11:55 +0900

    Title: 앞에서부터 삭제하기 2, Time: 2949ms, Memory: 23MB, Status: Wrong Answer - CodeTree

---
+++ 250204/앞에서부터 삭제하기 2/delete-it-from-the-beginning-2.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[n];
+        int sum = 0;
+        st = new StringTokenizer(br.readLine());
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+            sum += arr[i];
+            pq.add(arr[i]);
+        }
+
+        double max = Double.MIN_VALUE;
+
+        for (int i = 0; i < n - 2; i++) {
+            sum -= arr[i];
+            pq.remove(arr[i]);
+
+            int avg = (sum - pq.peek()) / (pq.size() - 1);
+
+            max = Math.max(avg, max);
+        }
+
+        System.out.printf("%.2f", max);
+    }
+}
