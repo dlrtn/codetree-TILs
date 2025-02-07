0462007ed8841405df0746588835d6c451108db4
Icksoo <87226112+dlrtn@users.noreply.github.com>
2025-02-07 13:58:13 +0900

    Title: 최솟값 3개, Time: 694ms, Memory: 22MB, Status: Passed - CodeTree

--- 250207/최솟값 3개/top-3-smallest-number.java
+++ 250207/최솟값 3개/top-3-smallest-number.java
@@ -1,7 +1,9 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.LinkedList;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -12,18 +14,24 @@ public class Main {
 
         st = new StringTokenizer(br.readLine());
 
-        LinkedList<Integer> list = new LinkedList<>();
+        PriorityQueue<Integer> list = new PriorityQueue<>();
         for (int i = 0; i < n; i++) {
             int num = Integer.parseInt(st.nextToken());
 
             list.add(num);
 
             if (list.size() >= 3) {
-                list.sort(Comparator.naturalOrder());
+                LinkedList<Integer> temp = new LinkedList<>();
 
                 long mul = 1;
                 for (int j = 0; j < 3; j++) {
-                    mul *= list.get(j);
+                    int val = list.poll();
+                    mul *= val;
+                    temp.add(val);
+                }
+
+                for (int j = 0; j < 3; j++) {
+                    list.add(temp.poll());
                 }
 
                 System.out.println(mul);
