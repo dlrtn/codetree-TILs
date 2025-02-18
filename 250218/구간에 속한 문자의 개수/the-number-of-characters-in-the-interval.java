43b1fd6cbb6d83a640e584aff2295e19d71a391e
Icksoo <87226112+dlrtn@users.noreply.github.com>
2025-02-18 09:35:30 +0900

    Title: 구간에 속한 문자의 개수, Time: 4180ms, Memory: 41MB, Status: Time Limit Exceed - CodeTree

---
+++ 250218/구간에 속한 문자의 개수/the-number-of-characters-in-the-interval.java
@@ -0,0 +1,58 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        char[][] map = new char[n + 1][m + 1];
+        for (int i = 1; i <= n; i++) {
+            st = new StringTokenizer(br.readLine());
+            String line = st.nextToken();
+            for (int j = 1; j <= m; j++) {
+                map[i][j] = line.charAt(j - 1);
+            }
+        }
+
+        int[][] a_cnt = new int[n + 1][m + 1];
+        int[][] b_cnt = new int[n + 1][m + 1];
+        int[][] c_cnt = new int[n + 1][m + 1];
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                if (map[i][j] == 'a') {
+                    a_cnt[i][j]++;
+                } else if (map[i][j] == 'b') {
+                    b_cnt[i][j]++;
+                } else if (map[i][j] == 'c') {
+                    c_cnt[i][j]++;
+                }
+
+                a_cnt[i][j] += a_cnt[i - 1][j] + a_cnt[i][j - 1] - a_cnt[i - 1][j - 1];
+                b_cnt[i][j] += b_cnt[i - 1][j] + b_cnt[i][j - 1] - b_cnt[i - 1][j - 1];
+                c_cnt[i][j] += c_cnt[i - 1][j] + c_cnt[i][j - 1] - c_cnt[i - 1][j - 1];
+            }
+        }
+
+        for (int i = 0; i < k; i++) {
+            st = new StringTokenizer(br.readLine());
+            int x1 = Integer.parseInt(st.nextToken()) - 1;
+            int y1 = Integer.parseInt(st.nextToken()) - 1;
+            int x2 = Integer.parseInt(st.nextToken());
+            int y2 = Integer.parseInt(st.nextToken());
+
+            System.out.print(a_cnt[x2][y2] - a_cnt[x1][y2] - a_cnt[x2][y1] + a_cnt[x1][y1] + " ");
+            System.out.print(b_cnt[x2][y2] - b_cnt[x1][y2] - b_cnt[x2][y1] + b_cnt[x1][y1] + " ");
+            System.out.println(c_cnt[x2][y2] - c_cnt[x1][y2] - c_cnt[x2][y1] + c_cnt[x1][y1]);
+        }
+
+    }
+}
