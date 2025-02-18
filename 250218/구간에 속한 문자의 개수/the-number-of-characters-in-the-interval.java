8db3bb0fa1ce6395892440450dc335e4f3802802
Icksoo <87226112+dlrtn@users.noreply.github.com>
2025-02-18 09:38:12 +0900

    Title: 구간에 속한 문자의 개수, Time: 1240ms, Memory: 30MB, Status: Passed - CodeTree

--- 250218/구간에 속한 문자의 개수/the-number-of-characters-in-the-interval.java
+++ 250218/구간에 속한 문자의 개수/the-number-of-characters-in-the-interval.java
@@ -14,20 +14,17 @@ public class Main {
         int k = Integer.parseInt(st.nextToken());
 
         char[][] map = new char[n + 1][m + 1];
+
+        int[][] a_cnt = new int[n + 1][m + 1];
+        int[][] b_cnt = new int[n + 1][m + 1];
+        int[][] c_cnt = new int[n + 1][m + 1];
         for (int i = 1; i <= n; i++) {
             st = new StringTokenizer(br.readLine());
             String line = st.nextToken();
             for (int j = 1; j <= m; j++) {
                 map[i][j] = line.charAt(j - 1);
-            }
-        }
 
-        int[][] a_cnt = new int[n + 1][m + 1];
-        int[][] b_cnt = new int[n + 1][m + 1];
-        int[][] c_cnt = new int[n + 1][m + 1];
 
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= m; j++) {
                 if (map[i][j] == 'a') {
                     a_cnt[i][j]++;
                 } else if (map[i][j] == 'b') {
@@ -41,7 +38,7 @@ public class Main {
                 c_cnt[i][j] += c_cnt[i - 1][j] + c_cnt[i][j - 1] - c_cnt[i - 1][j - 1];
             }
         }
-
+        
         for (int i = 0; i < k; i++) {
             st = new StringTokenizer(br.readLine());
             int x1 = Integer.parseInt(st.nextToken()) - 1;
@@ -49,9 +46,11 @@ public class Main {
             int x2 = Integer.parseInt(st.nextToken());
             int y2 = Integer.parseInt(st.nextToken());
 
-            System.out.print(a_cnt[x2][y2] - a_cnt[x1][y2] - a_cnt[x2][y1] + a_cnt[x1][y1] + " ");
-            System.out.print(b_cnt[x2][y2] - b_cnt[x1][y2] - b_cnt[x2][y1] + b_cnt[x1][y1] + " ");
-            System.out.println(c_cnt[x2][y2] - c_cnt[x1][y2] - c_cnt[x2][y1] + c_cnt[x1][y1]);
+            int a = a_cnt[x2][y2] - a_cnt[x1][y2] - a_cnt[x2][y1] + a_cnt[x1][y1];
+            int b = b_cnt[x2][y2] - b_cnt[x1][y2] - b_cnt[x2][y1] + b_cnt[x1][y1];
+            int c = c_cnt[x2][y2] - c_cnt[x1][y2] - c_cnt[x2][y1] + c_cnt[x1][y1];
+
+            System.out.println(a + " " + b + " " + c);
         }
 
     }
