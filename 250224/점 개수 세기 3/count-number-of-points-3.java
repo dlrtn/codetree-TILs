650fb735010cedc42d02deb777854b8485f0f7b4
Icksoo <87226112+dlrtn@users.noreply.github.com>
2025-02-24 17:31:13 +0900

    Title: 점 개수 세기 3, Time: 843ms, Memory: 27MB, Status: Passed - CodeTree

---
+++ 250224/점 개수 세기 3/count-number-of-points-3.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+import java.util.TreeMap;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int q = Integer.parseInt(st.nextToken());
+
+        TreeMap<Integer, Integer> map = new TreeMap<>();
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            map.put(Integer.parseInt(st.nextToken()), i + 1);
+        }
+
+        for (int i = 0; i < q; i++) {
+            st = new StringTokenizer(br.readLine());
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+
+            int left = map.floorKey(start);
+            int right = map.ceilingKey(end);
+
+            System.out.println(map.get(right) - map.get(left) + 1);
+        }
+
+    }
+}
