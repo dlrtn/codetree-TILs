63d8ece35bfa72e5c53230e1270c4f4342d4f4cc
Icksoo <87226112+dlrtn@users.noreply.github.com>
2025-03-06 14:59:20 +0900

    Title: 겹치는 선분들, Time: 237ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 250306/겹치는 선분들/overlapping-lines.java
@@ -0,0 +1,71 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        int now = 0;
+        ArrayList<Point> points = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            int a = Integer.parseInt(st.nextToken());
+            String s = st.nextToken();
+
+            if (s.equals("R")) {
+                points.add(new Point(now, true));
+                points.add(new Point(now + a, false));
+                now += a;
+            } else {
+                points.add(new Point(now - a, true));
+                points.add(new Point(now, false));
+                now -= a;
+            }
+        }
+
+        points.sort(Comparator.comparingInt(p -> p.x));
+
+        int count = 0;
+        int answer = 0;
+        for (int i = 0; i < n * 2; i++) {
+            int x = points.get(i).x;
+            boolean isStart = points.get(i).isStart;
+
+            if (isStart) {
+                count++;
+            } else {
+                count--;
+            }
+
+            if (count >= k) {
+                answer++;
+            }
+        }
+
+        System.out.println(answer);
+    }
+
+    public static class Point implements Comparable<Point> {
+        int x;
+        boolean isStart;
+
+        public Point(int x, boolean isStart) {
+            this.x = x;
+            this.isStart = isStart;
+        }
+
+        @Override
+        public int compareTo(Point o) {
+            return this.x - o.x;
+        }
+    }
+}
