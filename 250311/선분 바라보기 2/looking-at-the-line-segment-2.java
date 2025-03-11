992c37861ab629e0436aef97d4803ac397f4c506
Icksoo <87226112+dlrtn@users.noreply.github.com>
2025-03-11 16:00:17 +0900

    Title: 선분 바라보기 2, Time: 774ms, Memory: 25MB, Status: Passed - CodeTree

--- 250311/선분 바라보기 2/looking-at-the-line-segment-2.java
+++ 250311/선분 바라보기 2/looking-at-the-line-segment-2.java
@@ -1,57 +1,95 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.StringTokenizer;
+import java.util.TreeMap;
 
 public class Main {
+
     public static void main(String[] args) throws IOException {
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int n = Integer.parseInt(st.nextToken());
+        int n = Integer.parseInt(br.readLine());
 
-        ArrayList<Point> list = new ArrayList<>();
+        ArrayList<Pair> pair = new ArrayList<>();
+
+        boolean[] checked = new boolean[n];
+        int controalTower = -1;
         for (int i = 0; i < n; i++) {
-            st = new StringTokenizer(br.readLine());
+            StringTokenizer st = new StringTokenizer(br.readLine());
 
             int y = Integer.parseInt(st.nextToken());
             int x1 = Integer.parseInt(st.nextToken());
             int x2 = Integer.parseInt(st.nextToken());
 
-            list.add(new Point(x1, y, true, i + 1));
-            list.add(new Point(x2, y, false, i + 1));
+            pair.add(new Pair(x1, y, 1, i));
+            pair.add(new Pair(x2, y, -1, i));
         }
+        Collections.sort(pair);
 
-        list.sort(Comparator.comparingInt(p -> p.x));
+        TreeMap<Integer, Integer> tMap = new TreeMap<>();
 
-        PriorityQueue<Integer> set = new PriorityQueue<>();
-        Set<Integer> answer = new HashSet<>();
-        for (Point point : list) {
-            if (point.isStarted) {
-                set.add(point.index);
+        int minIdx = 0;
+        for (int i = 0; i < n * 2; i++) {
+            int xDot = pair.get(i).a;
+            int yDot = pair.get(i).b;
+            int loc = pair.get(i).y;
+            int idx = pair.get(i).idx;
+
+            if (loc == 1) {
+                if (tMap.size() == 0) {
+                    checked[idx] = true;
+                } else if (tMap.firstKey() > yDot) {
+                    checked[idx] = true;
+                }
+
+                tMap.put(yDot, idx);
+
             } else {
-                set.remove(point.index);
-            }
+                tMap.remove(yDot);
 
-            if (!set.isEmpty() && !answer.contains(set.peek())) {
-                answer.add(point.index);
+                if (tMap.size() != 0) {
+                    if (!checked[tMap.get(tMap.firstKey())]) {
+                        checked[tMap.get(tMap.firstKey())] = true;
+                    }
+                } else {
+                    if (!checked[idx]) {
+                        checked[idx] = true;
+                    }
+                }
+
+
+            }
+        }
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+            if (checked[i]) {
+                count++;
             }
         }
 
-        System.out.println(answer.size());
+        System.out.println(count);
     }
 
-    public static class Point {
-        int x;
+    public static class Pair implements Comparable<Pair> {
+        int a;
+        int b;
         int y;
-        boolean isStarted;
-        int index;
+        int idx;
 
-        public Point(int x, int y, boolean isStarted, int index) {
-            this.x = x;
+        Pair(int a, int b, int y, int idx) {
+            this.a = a;
+            this.b = b;
             this.y = y;
-            this.isStarted = isStarted;
-            this.index = index;
+            this.idx = idx;
+        }
+
+        @Override
+        public int compareTo(Pair p) {
+            return this.a - p.a;
         }
     }
 }
