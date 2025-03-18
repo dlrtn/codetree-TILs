903df729d81605d1a7b86194edfdeead9c87cf1a
Icksoo <87226112+dlrtn@users.noreply.github.com>
2025-03-18 16:21:09 +0900

    Title: 가장 짧은 부분합, Time: 3024ms, Memory: 17MB, Status: Time Limit Exceed - CodeTree

--- 250318/가장 짧은 부분합/shortest-subtotal.java
+++ 250318/가장 짧은 부분합/shortest-subtotal.java
@@ -10,28 +10,28 @@ public class Main {
 
         int n = Integer.parseInt(st.nextToken());
         int s = Integer.parseInt(st.nextToken());
-        
+
         int[] arr = new int[n];
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < n; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
 
-        long[] sum = new long[n];
-        sum[0] = arr[0];
-        for (int i = 1; i < n; i++) {
-            sum[i] = sum[i - 1] + arr[i];
-        }
-
         int min = Integer.MAX_VALUE;
         for (int i = 0; i < n; i++) {
-            for (int j = i + 1; j < n; j++) {
-                if (sum[j] - sum[i] >= s) {
-                    min = Math.min(min, j - i);
+            long sum = 0;
+            for (int j = i; j < n; j++) {
+                sum += arr[j];
+                if (sum >= s) {
+                    min = Math.min(min, j - i + 1);
                 }
             }
         }
 
-        System.out.println(min);
+        if (min == Integer.MAX_VALUE) {
+            System.out.println("-1");
+        } else {
+            System.out.println(min);
+        }
     }
 }
