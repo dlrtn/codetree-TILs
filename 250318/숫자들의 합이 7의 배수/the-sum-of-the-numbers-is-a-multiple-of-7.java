6be8c4366d6dd72970aab745353d0bfa165ef162
Icksoo <87226112+dlrtn@users.noreply.github.com>
2025-03-18 15:36:33 +0900

    Title: 숫자들의 합이 7의 배수, Time: 2642ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 250318/숫자들의 합이 7의 배수/the-sum-of-the-numbers-is-a-multiple-of-7.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int[] sum = new int[n];
+        sum[0] = arr[0];
+        for (int i = 1; i < n; i++) {
+            sum[i] = sum[i - 1] + arr[i];
+        }
+
+        int max = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if ((sum[j] - sum[i]) % 7 == 0) {
+                    max = Math.max(max, j - i);
+                }
+            }
+        }
+
+        System.out.println(max);
+    }
+}
