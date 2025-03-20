5df7c84ec1d9c66a85e7b75f0b1cdbd8f588fdc1
Icksoo <87226112+dlrtn@users.noreply.github.com>
2025-03-20 14:47:03 +0900

    Title: 부분수열 여부 판단하기, Time: 215ms, Memory: 18MB, Status: Passed - CodeTree

---
+++ 250320/부분수열 여부 판단하기/determine-subsequence.java
@@ -0,0 +1,45 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        int[] A = new int[n];
+        int[] B = new int[m];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            A[i] = Integer.parseInt(st.nextToken());
+        }
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < m; i++) {
+            B[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int i = 0, j = 0;
+        while (i < n && j < m) {
+            if (A[i] == B[j]) {
+                i++;
+                j++;
+            } else {
+                i++;
+            }
+        }
+
+        if (j == m) {
+            System.out.println("Yes");
+        } else {
+            System.out.println("No");
+        }
+    }
+}
