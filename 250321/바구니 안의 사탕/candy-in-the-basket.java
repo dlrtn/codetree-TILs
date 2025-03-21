7cf6497024e07bf11a2c8713c92729888a91b2e8
Icksoo <87226112+dlrtn@users.noreply.github.com>
2025-03-21 15:10:18 +0900

    Title: 바구니 안의 사탕, Time: 97ms, Memory: 9MB, Status: Runtime Error - CodeTree

---
+++ 250321/바구니 안의 사탕/candy-in-the-basket.java
@@ -0,0 +1,45 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int[] arr = new int[100002];
+
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            int candyCount = Integer.parseInt(st.nextToken());
+            int point = Integer.parseInt(st.nextToken());
+
+            arr[point] += candyCount;
+        }
+
+        int max = 0;
+        int i = 0, j = 0;
+        int count = 0;
+        while (j < 100001) {
+            while (j < 100001 && j - i < 2 * k) {
+                count += arr[j];
+                j++;
+            }
+
+            max = Math.max(max, count);
+
+            count -= arr[i];
+            i++;
+            j++;
+            count += arr[j];
+        }
+
+        System.out.println(max);
+
+    }
+}
