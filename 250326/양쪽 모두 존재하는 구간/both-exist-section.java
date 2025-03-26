8ceaa3677f87045fca846559d461e7d90ef27343
Icksoo <87226112+dlrtn@users.noreply.github.com>
2025-03-26 16:42:32 +0900

    Title: 양쪽 모두 존재하는 구간, Time: 89ms, Memory: 9MB, Status: Wrong Answer - CodeTree

--- 250326/양쪽 모두 존재하는 구간/both-exist-section.java
+++ 250326/양쪽 모두 존재하는 구간/both-exist-section.java
@@ -1,7 +1,9 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -9,7 +11,7 @@ public class Main {
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         int n = Integer.parseInt(st.nextToken());
-        int k = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
 
         st = new StringTokenizer(br.readLine());
         int[] arr = new int[n];
@@ -21,20 +23,37 @@ public class Main {
             originalMap.put(arr[i], originalMap.getOrDefault(arr[i], 0) + 1);
         }
 
-        int answer = Integer.MIN_VALUE;
+        for (Integer key : originalMap.keySet()) {
+            if (originalMap.get(key) == 1) {
+                System.out.println(-1);
+                return;
+            }
+        }
+
+        int answer = Integer.MAX_VALUE;
 
         Map<Integer, Integer> map = new HashMap<>();
         int j = 0;
         for (int i = 0; i < n; i++) {
             while (j < n && originalMap.get(arr[j]) != 1) {
                 map.put(arr[j], map.getOrDefault(arr[j], 0) + 1);
-                originalMap.put(arr[j], originalMap.getOrDefault(arr[j], 0) - 1);
+                if (originalMap.get(arr[j]) == 1) {
+                    originalMap.remove(arr[j]);
+                } else {
+                    originalMap.put(arr[j], originalMap.getOrDefault(arr[j], 0) - 1);
+                }
                 j++;
             }
 
-            answer = Math.max(answer, j - i);
+            if (originalMap.size() == map.size() && map.size() == m) {
+                answer = Math.min(answer, j - i);
+            }
 
-            map.put(arr[i], map.getOrDefault(arr[i], 0) - 1);
+            if (map.get(arr[i]) == 1) {
+                map.remove(arr[i]);
+            } else {
+                map.put(arr[i], map.getOrDefault(arr[i], 0) - 1);
+            }
             originalMap.put(arr[i], originalMap.getOrDefault(arr[i], 0) + 1);
         }
 
