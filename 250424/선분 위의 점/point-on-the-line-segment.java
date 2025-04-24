b38d53e71f7b55626f81d348cdf8e988d0b113c1
Icksoo <87226112+dlrtn@users.noreply.github.com>
2025-04-24 09:48:42 +0900

    Title: 선분 위의 점, Time: 269ms, Memory: 15MB, Status: Wrong Answer - Codetree

---
+++ 250424/선분 위의 점/point-on-the-line-segment.java
@@ -0,0 +1,65 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[n];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+
+            int lowerBound = lowerBound(arr, start, n);
+            int upperBound = upperBound(arr, end, n);
+
+            System.out.println(upperBound - lowerBound);
+        }
+    }
+
+    public static int upperBound(int[] arr, int targetNumber, int n) {
+        int left = 0;
+        int right = n - 1;
+        int minIdx = n;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (arr[mid] > targetNumber) {
+                right = mid - 1;
+                minIdx = Math.min(minIdx, mid);
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        return minIdx;
+    }
+
+    public static int lowerBound(int[] arr, int targetNumber, int n) {
+        int left = 0;
+        int right = n - 1;
+        int minIdx = n;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (arr[mid] >= targetNumber) {
+                right = mid - 1;
+                minIdx = Math.min(minIdx, mid);
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        return minIdx;
+    }
+}
