d9cd2535eaea385e6db85b425e9f7fbc0778f597
Icksoo <87226112+dlrtn@users.noreply.github.com>
2025-04-28 00:26:30 +0900

    Title: 자연수 n개의 합, Time: 117ms, Memory: 10MB, Status: Passed - Codetree

--- 250428/자연수 n개의 합/sum-of-n-natural-numbers.java
+++ 250428/자연수 n개의 합/sum-of-n-natural-numbers.java
@@ -1,45 +1,30 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.Scanner;
 
-public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+public class Main {    
+    public static final int MAX_S = 2000000000;
+    
+    // 변수 선언
+    public static long s;
 
-        long n = Long.parseLong(st.nextToken());
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // 입력:
+        s = sc.nextLong();
 
-        System.out.println(parametricSearch(n));
-    }
+        long left = 1;                          // 가장 작은 숫자 값을 설정합니다.
+        long right = MAX_S;                     // 가장 큰 숫자 값을 설정합니다.
+        long maxNum = 0;                        // 최대이므로, 답이 될 수 있는 값보다 더 작은 값으로 설정합니다.
 
-    public static long parametricSearch(long n) {
-        long l = 0;
-        long r = n;
-
-        while (l < r) {
-            long mid = (l + r) / 2;
-
-            long sum = calculateSum(mid);
-
-            if (sum > n) {
-                r = mid;
-            } else {
-                l = mid + 1;
+        while (left <= right) {                 // [left, right]가 유효한 구간이면 계속 수행합니다.
+            long mid = (left + right) / 2;      // 가운데 위치를 선택합니다.
+            if(mid * (mid + 1) / 2 <= s) {      // 1부터 n까지의 합이 s보다 같거나 작다면
+                left = mid + 1;                 // 오른쪽에 조건을 만족하는 숫자가 더 있을 가능성 때문에 left를 바꿔줍니다.
+                maxNum = Math.max(maxNum, mid); // 답의 후보들 중 최댓값을 계속 갱신해줍니다.
             }
-        }
-        return l - 1;
-    }
-
-    public static long calculateSum(long n) {
-        long sum = 0;
-
-        if (n % 2 == 0) {
-            sum = (n / 2) * (n + 1);
-        } else {
-            sum = ((n + 1) / 2) * n;
+            else                               
+                right = mid - 1;                // s보다 큰 경우라면 right를 바꿔줍니다.
         }
 
-        return sum;
+        System.out.print(maxNum);               // 조건을 만족하는 최대 n의 값을 반환합니다.
     }
 }
